12350a1f7492e85d86b1ef64bd5be6d9a44d3124
nikzaugg <zauggnik@gmail.com>
2019-10-22 09:32:58 +0200

    Client - zokrates-js with working ganache blockchain for local dev

---
+++ zokrates-js-eth/client/utils/getWeb3.js
@@ -0,0 +1,35 @@
+import Web3 from "web3";
+
+const getWeb3 = async () =>
+  new Promise((resolve, reject) => {
+      // Modern dapp browsers...
+      if (window.ethereum) {
+        const web3 = new Web3(window.ethereum);
+        try {
+          // Request account access if needed
+          window.ethereum.enable();
+          // Acccounts now exposed
+          resolve(web3);
+        } catch (error) {
+          reject(error);
+        }
+      }
+      // Legacy dapp browsers...
+      else if (window.web3) {
+        // Use Mist/MetaMask's provider.
+        const web3 = window.web3;
+        console.log("Injected web3 detected.");
+        resolve(web3);
+      }
+      // Fallback to localhost; use dev console port by default...
+      else {
+        const provider = new Web3.providers.HttpProvider(
+          "http://127.0.0.1:9545"
+        );
+        const web3 = new Web3(provider);
+        console.log("No web3 instance injected, using Local web3.");
+        resolve(web3);
+      }
+  });
+
+export default getWeb3;
