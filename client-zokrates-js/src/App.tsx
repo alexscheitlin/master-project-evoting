24c338aa44709bc34939010febd2636fd9828635
nikzaugg <zauggnik@gmail.com>
2019-10-22 08:19:48 +0200

    Client - zokrates-js works

--- client-zokrates-js/src/App.tsx
+++ client-zokrates-js/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { hot } from 'react-hot-loader';
 import './App.css';
 import * as zokrates from 'zokrates-js';
@@ -7,10 +7,14 @@ interface Props {}
 
 const App: React.FC<Props> = () => {
   const [solidityContract, setSolidityContract] = useState();
+  const [key, setKey] = useState();
 
-  const code = `def main(private field a, field b) -> (field):
-  field result = if a * a == b then 1 else 0 fi
-  return result`;
+  const code = `
+  def main(private field a) -> (field):
+    field result = if a == 0 then 1 else 0 fi
+    return result
+  `;
+
   function importResolver(location, path) {
     // implement your resolving logic here
     return {
@@ -19,28 +23,35 @@ const App: React.FC<Props> = () => {
     };
   }
 
-  zokrates.initialize(importResolver).then(() => {
-    // we have to initialize wasm module before calling api functions
+  useEffect(() => {
+    zokrates.initialize(importResolver).then(() => {
+      // we have to initialize wasm module before calling api functions
 
-    // compile the program code
-    const program = zokrates.compile(code, './root.zok');
+      // compile the program code
+      const program = zokrates.compile(code, 'main');
 
-    // generate the verifier key and proving key
-    // => this should eventually be provided by the system
-    const [verifierKey, provingKey] = zokrates.setup(program);
+      // generate the verifier key and proving key
+      // => this should eventually be provided by the system
+      const keys = zokrates.setup(program);
 
-    // generate a witness for the proof
-    const witness = zokrates.computeWitness(program, ['2', '4']);
+      const verifierKey = keys[0];
+      const provingKey = keys[1];
+      setKey(provingKey);
+      console.log(Array.from(provingKey));
 
-    // generate the proof
-    const proof = zokrates.generateProof(program, witness, provingKey);
+      // generate a witness for the proof
+      const witness = zokrates.computeWitness(program, ['1']);
 
-    // generate a solidity contract, this should eventually be deployed
-    // by the system beforehand
-    const contract = zokrates.exportSolidityVerifier(verifierKey, true);
-    setSolidityContract(contract);
-    console.log(contract);
-  });
+      // generate the proof
+      const proof = zokrates.generateProof(program, witness, provingKey);
+
+      // generate a solidity contract, this should eventually be deployed
+      // by the system beforehand
+      const contract = zokrates.exportSolidityVerifier(verifierKey, true);
+      setSolidityContract(contract);
+    });
+    return () => {};
+  }, []);
 
   return (
     <div>
