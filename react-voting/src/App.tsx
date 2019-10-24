174ab4c565d8e8c382e58df9f0d69abe29de2c3a
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2019-10-24 13:25:06 +0200

    add ecc elgamal to react app (not yet working)

--- react-voting/src/App.tsx
+++ react-voting/src/App.tsx
@@ -1,60 +1,18 @@
-import React, { useState } from 'react';
-
-import { ElGamal, ElGamalVoting, PublicKey, Cipher, Summary } from 'mp-crypto';
+import React from 'react';
 
 import './App.css';
-
-const [pk, sk] = ElGamal.generateKeys(137, 51)
+import ElGamalComponent from './components/ElGamal';
+import EccElGamalComponent from './components/EccElGamal';
 
 const App: React.FC = () => {
 
-  const [votes, setVotes] = useState<Cipher[]>([]);
-  const [result, setResult] = useState<number>(0);
-  const [summary, setSummary] = useState<Summary>({ total: 0, yes: 0, no: 0 });
-
-  const addYesVote = () => {
-    const newVotes = [...votes, ElGamalVoting.generateYesVote(pk)]
-    setVotes(newVotes)
-    getResult(newVotes)
-  }
-
-  const addNoVote = () => {
-    const newVotes = [...votes, ElGamalVoting.generateNoVote(pk)]
-    setVotes(newVotes)
-    getResult(newVotes)
-  }
-
-  const getResult = (votes: any[]) => {
-    const result = ElGamalVoting.tallyVotes(pk, sk, votes)
-    setResult(result)
-
-    const sum = ElGamalVoting.getSummary(votes.length, result)
-    setSummary(sum)
-  }
-
   return (
     <div className="App">
-      <header className="App-header">
-        <button className="btn" onClick={addYesVote}>Vote Yes</button> <button className="btn" onClick={addNoVote}>Vote No</button>
-        <br></br>
-        <br></br>
-
-        Public Key: p={pk.p.toNumber()} g={pk.g.toNumber()} h={pk.h.toNumber()}
-        <br></br>
-        Private Key: {sk.toNumber()}
-
-        <br></br>
-        <br></br>
-
-        Total: {summary.total} Yes: {summary.yes} No: {summary.no}
-
-        <br></br>
-        <br></br>
-
-        Votes: {votes.length}
-        {votes.map((vote, i) => (
-          <div key={i} >{vote.c1.toNumber()}, {vote.c2.toNumber()} - {ElGamal.decrypt1(vote, sk, pk).toNumber()}</div>
-        ))}
+      <header className="App-header" style={{ display: 'flex' }}>
+        <div style={{width: '50%' }}>
+          <ElGamalComponent></ElGamalComponent></div>
+        <div style={{width: '50%' }}>
+          <EccElGamalComponent></EccElGamalComponent></div>
       </header>
     </div>
   );
---
+++ react-voting/src/components/EccElGamal.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+
+import { EccElGamal, EccElGamalVoting, Cipher, Summary } from 'mp-crypto';
+const EC = require('elliptic').ec
+const ec = new EC('secp256k1')
+
+const keyPair = ec.genKeyPair()
+const privateKey = keyPair.getPrivate()
+const publicKey = keyPair.getPublic()
+
+const EccElGamalComponent: React.FC = () => {
+
+  const [votes, setVotes] = useState<Cipher[]>([]);
+  const [result, setResult] = useState<number>(0);
+  const [summary, setSummary] = useState<Summary>({ total: 0, yes: 0, no: 0 });
+
+  const addYesVote = () => {
+    const newVotes = [...votes, EccElGamalVoting.generateYesVote(publicKey)]
+    setVotes(newVotes)
+    getResult(newVotes)
+  }
+
+  const addNoVote = () => {
+    const newVotes = [...votes, EccElGamalVoting.generateNoVote(publicKey)]
+    setVotes(newVotes)
+    getResult(newVotes)
+  }
+
+  const getResult = (votes: any[]) => {
+    const result = EccElGamalVoting.tallyVotes(publicKey, privateKey, votes)
+    setResult(result)
+
+    const sum = EccElGamalVoting.getSummary(votes.length, result)
+    setSummary(sum)
+  }
+  
+  return (
+    <div>
+      <button className="btn" onClick={addYesVote}>Vote Yes</button> <button className="btn" onClick={addNoVote}>Vote No</button>
+      <br></br>
+      <br></br>
+
+      Public Key: {JSON.parse(JSON.stringify(publicKey))[0]} {JSON.parse(JSON.stringify(publicKey))[1]}
+      <br></br>
+      Private Key: {JSON.stringify(privateKey)}
+
+      <br></br>
+      <br></br>
+
+      Total: {summary.total} Yes: {summary.yes} No: {summary.no}
+
+      <br></br>
+      <br></br>
+
+      Votes: {votes.length}
+      {votes.map((vote, i) => (
+        <div key={i} >{vote.c1.toNumber()}, {vote.c2.toNumber()} - {JSON.stringify(EccElGamal.decrypt(vote, privateKey))}</div>
+      ))}
+    </div>
+  );
+}
+
+export default EccElGamalComponent;
