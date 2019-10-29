4f5a91e0d567ac4540b7f6afb38e41c8914698d2
nikzaugg <zauggnik@gmail.com>
2019-10-29 17:18:45 +0100

    truffle - added article how to call .sol function with BN

--- solidity-contract-testing/test/verifier.js
+++ solidity-contract-testing/test/verifier.js
@@ -1,5 +1,8 @@
 const Verifier = artifacts.require('Verifier');
 
+// article that explains how to pass 256uints to solidity functions
+// https://ethereum.stackexchange.com/questions/63399/bytes32-uint256-conversion
+
 contract('Verifier', accounts => {
   it('2 + 2 = 4', async () => {
     const verifierInstance = await Verifier.deployed();
