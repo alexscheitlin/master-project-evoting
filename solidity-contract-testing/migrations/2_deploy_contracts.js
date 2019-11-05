b420d265680c0d5bb6b9e19a15817d1894e74161
nikzaugg <zauggnik@gmail.com>
2019-11-05 22:26:29 +0100

    finite field verifier contract added - added first implementation - tests do not pass yet, but arguments can be passed to the contract - simple test das verifies a proof

--- solidity-contract-testing/migrations/2_deploy_contracts.js
+++ solidity-contract-testing/migrations/2_deploy_contracts.js
@@ -1,9 +1,11 @@
 const EllipticLib = artifacts.require('EllipticCurve');
 const Verifier = artifacts.require('Verifier');
+const FiniteFieldVerifier = artifacts.require('FiniteFieldVerifier');
 
 module.exports = function(deployer) {
   // TODO: find out what .link does and if it's needed
   deployer.deploy(EllipticLib);
   deployer.link(EllipticLib, Verifier);
   deployer.deploy(Verifier);
+  deployer.deploy(FiniteFieldVerifier);
 };
---
+++ solidity-contract-testing/test/finiteFieldVerifier.js
@@ -0,0 +1,59 @@
+const FiniteFieldVerifier = artifacts.require('FiniteFieldVerifier');
+
+// this public key is set in the constructor in the smart contract
+// for testing purposes, the key is now set manually in the contract
+// to ensure that the tests pass, these values must be the same as the
+// values in the constructor of FiniteFieldVerifier.sol
+const PublicKey = {
+  p: '', // prime
+  q: '', // prime factor: p = 2*q+1
+  g: '', // generator
+  h: ''
+};
+
+const cipher = {
+  a: '',
+  b: ''
+};
+
+const proof = {
+  a0: '',
+  a1: '',
+  b0: '',
+  b1: '',
+  c0: '',
+  c1: '',
+  f0: '',
+  f1: ''
+};
+
+// the ETH address of the sender of the proof
+// used to add more security in the hash function as it is unique
+const ethAddressOfSender = '0xad4e7d8f03904b175a1f8ae0d88154f329ac9329';
+
+contract('FiniteFieldVerifier.sol', () => {
+  const _cipher0 = web3.utils.toBN(cipher.a);
+  const _cipher1 = web3.utils.toBN(cipher.b);
+  const _a0 = web3.utils.toBN(proof.a0);
+  const _a1 = web3.utils.toBN(proof.a1);
+  const _b0 = web3.utils.toBN(proof.b0);
+  const _b1 = web3.utils.toBN(proof.b1);
+  const _c0 = web3.utils.toBN(proof.c0);
+  const _c1 = web3.utils.toBN(proof.c1);
+  const _f0 = web3.utils.toBN(proof.f0);
+  const _f1 = web3.utils.toBN(proof.f1);
+
+  it('Finite Field ElGamal Proof can be verified on the blockchain', async () => {
+    const verifierInstance = await FiniteFieldVerifier.deployed();
+
+    const res = await verifierInstance.verifyProof(
+      [_cipher0, _cipher1],
+      [_a0, _a1],
+      [_b0, _b1],
+      [_c0, _c1],
+      [_f0, _f1]
+    );
+
+    assert.isTrue(res, 'proof could not be verified by the contract');
+  });
+});
