73c24d39adfda231a96808402a243c853e152025
nikzaugg <zauggnik@gmail.com>
2019-11-06 16:24:34 +0100

    solidity - add SumProofVerifier - verifier contract - test cases

--- solidity-contract-testing/migrations/2_deploy_contracts.js
+++ solidity-contract-testing/migrations/2_deploy_contracts.js
@@ -1,6 +1,7 @@
 const EllipticLib = artifacts.require('EllipticCurve');
 const Verifier = artifacts.require('Verifier');
 const VoteProofVerifier = artifacts.require('VoteProofVerifier');
+const SumProofVerifier = artifacts.require('SumProofVerifier');
 
 const params = require('./constructor_params');
 
@@ -11,6 +12,13 @@ module.exports = function(deployer) {
   deployer.deploy(Verifier);
   deployer.deploy(
     VoteProofVerifier,
+    params.params.ff.PublicKey.p,
+    params.params.ff.PublicKey.q,
+    params.params.ff.PublicKey.h,
+    params.params.ff.PublicKey.g
+  );
+  deployer.deploy(
+    SumProofVerifier,
     params.params.ff.PublicKey.p,
     params.params.ff.PublicKey.q,
     params.params.ff.PublicKey.h,
---
+++ solidity-contract-testing/test/sumProof.spec.js
@@ -0,0 +1,65 @@
+const SumProofVerifier = artifacts.require('SumProofVerifier');
+const { FFelGamal } = require('../crypto');
+const random = require('random');
+
+// this is a ETH address
+// is needed for creating a proof
+// this address is currently hardcoded
+// in the future, this would be set as the currently active wallet address
+const uniqueID = '0x71C7656EC7ab88b098defB751B7401B5f6d8976F';
+
+const testCases = [[23, 2], [23, 6], [23, 8]];
+
+contract('SumProofVerifier.sol', () => {
+  // run 10 tests for each test case
+  for (let i = 0; i < 10; i++) {
+    for (const testCase of testCases) {
+      it(`ZKP Sum Verification for (p: ${testCase[0]}, g: ${
+        testCase[1]
+      })`, async () => {
+        const p = testCase[0];
+        const g = testCase[1];
+        let pk;
+        let sk;
+        try {
+          [pk, sk] = FFelGamal.Encryption.generateKeys(p, g);
+        } catch (error) {
+          console.error(error);
+        }
+
+        // deploy contract and pass system parameters
+        const verifierInstance = await SumProofVerifier.new(
+          pk.p,
+          pk.q,
+          pk.g,
+          pk.h
+        );
+
+        const sum = random.int(1, (p - 1) / 2 - 1);
+
+        const sumEnc = FFelGamal.Encryption.encrypt(sum, pk);
+        const proof = FFelGamal.SumZKP.generateSumProof(
+          sumEnc,
+          pk,
+          sk,
+          uniqueID
+        );
+
+        // const verifiedSumProof = FFelGamal.SumZKP.verifySumProof(sumEnc, proof, pk, uniqueID)
+        const verifiedSumProof = await verifierInstance.verifyProof(
+          sumEnc.a,
+          sumEnc.b,
+          proof.a1,
+          proof.b1,
+          proof.d,
+          proof.f
+        );
+
+        assert.isTrue(
+          verifiedSumProof,
+          'Sum proof could not be verified by the contract'
+        );
+      });
+    }
+  }
+});
