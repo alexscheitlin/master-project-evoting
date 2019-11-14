d54211fab37f68f3acd9d517d0d8f9ea99c30d12
nikzaugg <zauggnik@gmail.com>
2019-11-14 08:47:22 +0100

    voter frontend - remove ChainInfo component

--- voter-frontend/src/App.tsx
+++ voter-frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import { ChainInfo } from './components/ChainInfo';
 import { Navigation } from './components/Navigation';
 import { Routes } from './Router';
 
@@ -9,7 +8,6 @@ const App: React.FC = () => {
   return (
     <Router>
       <Navigation />
-      <ChainInfo />
       <Routes />
     </Router>
   );
