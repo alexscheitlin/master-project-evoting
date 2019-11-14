78ed4baeb274655c01990b215267fd3566fff007
nikzaugg <zauggnik@gmail.com>
2019-11-14 16:04:57 +0100

    voter frontend - end Vote - get votes from contract and add them - create proof locally for sum

--- voter-frontend/src/components/PlayTrough.tsx
+++ voter-frontend/src/components/PlayTrough.tsx
@@ -1,4 +1,4 @@
-import { FFelGamal, Cipher } from 'mp-crypto';
+import { FFelGamal, Cipher, ValidVoteProof } from 'mp-crypto';
 import React, { useState } from 'react';
 
 import ballotABI from '../contracts/Ballot.json';
@@ -12,21 +12,12 @@ const PlayTrough: React.FC = () => {
   const [web3, ballot] = useWeb3(ballotABI);
 
   // set by bund and only known by bund
-  const [systemParameters, setSystemParameters] = useState();
   const [privateKey, setPrivateKey] = useState();
-  const [publicKey, setPublicKey] = useState();
   const [systemParametersLoaded, setSystemParametersLoaded] = useState(false);
   const [verifiersLoaded, setVerifiersLoaded] = useState(false);
 
   // set by kanton locally
-  const [localSystemParams, setLocalSystemParams] = useState();
-  const [localPublicKey, setLocalPublicKey] = useState();
-  const [localSystemParamsLoaded, SetLocalSystemParamsLoaded] = useState(false);
-  const [localPublicKeyLoaded, setLocalPublicKeyLoaded] = useState(false);
   const [proofVerified, setProofVerified] = useState(false);
-
-  // local store -> move to contract later
-  const [votes, setVotes] = useState<Cipher[]>([]);
 
   const generateAndSetSystemParams = async () => {
     const keys = Encryption.generateKeysZKP(23, 2);
@@ -34,12 +25,6 @@ const PlayTrough: React.FC = () => {
     const sk = keys[1];
 
     // set local state
-    setSystemParameters({
-      p: pk.p,
-      q: pk.q,
-      g: pk.g,
-    });
-    setPublicKey(pk.h);
     setPrivateKey(sk);
 
     // set in contracts
@@ -50,91 +35,85 @@ const PlayTrough: React.FC = () => {
     setSystemParametersLoaded(true);
   };
 
-  const getSystemParams = async () => {
-    const systemParameters = await ballot.getParameters();
-    setLocalSystemParams(systemParameters);
-    SetLocalSystemParamsLoaded(true);
-  };
-
-  const getPublicKey = async () => {
-    const publicKey = await ballot.getPublicKey();
-    setLocalPublicKey(publicKey);
-    setLocalPublicKeyLoaded(true);
-  };
-
   const createVerifiers = async () => {
     await ballot.createVerifiers({ from: defaultAccount });
     setVerifiersLoaded(true);
   };
 
-  const castYesVote = async () => {
+  const castVote = async (type: number) => {
+    const systemParameters = await ballot.getParameters();
+    const publicKey = await ballot.getPublicKey();
+
     const params: FFelGamal.PublicKey = {
-      p: localSystemParams[0],
-      q: localSystemParams[1],
-      g: localSystemParams[2],
-      h: localPublicKey,
+      p: systemParameters[0],
+      q: systemParameters[1],
+      g: systemParameters[2],
+      h: publicKey,
     };
 
-    const vote = Voting.generateYesVote(params);
-    const walletAddress = defaultAccount;
-    const proof = VoteZKP.generateYesProof(vote, params, walletAddress);
+    let vote: Cipher;
+    let proof: ValidVoteProof;
+    if (type === 0) {
+      vote = Voting.generateNoVote(params);
+      proof = VoteZKP.generateNoProof(vote, params, defaultAccount);
+    } else {
+      vote = Voting.generateYesVote(params);
+      proof = VoteZKP.generateYesProof(vote, params, defaultAccount);
+    }
 
-    const yesBallot = await ballot.vote(
-      [web3.utils.toBN(vote.a).toString(), web3.utils.toBN(vote.b).toString()],
-      [web3.utils.toBN(proof.a0).toString(), web3.utils.toBN(proof.a1).toString()],
-      [web3.utils.toBN(proof.b0).toString(), web3.utils.toBN(proof.b1).toString()],
-      [web3.utils.toBN(proof.c0).toString(), web3.utils.toBN(proof.c1).toString()],
-      [web3.utils.toBN(proof.f0).toString(), web3.utils.toBN(proof.f1).toString()],
-      walletAddress,
-      { from: defaultAccount },
-    );
+    await ballot
+      .vote(
+        [web3.utils.toBN(vote.a).toString(), web3.utils.toBN(vote.b).toString()],
+        [web3.utils.toBN(proof.a0).toString(), web3.utils.toBN(proof.a1).toString()],
+        [web3.utils.toBN(proof.b0).toString(), web3.utils.toBN(proof.b1).toString()],
+        [web3.utils.toBN(proof.c0).toString(), web3.utils.toBN(proof.c1).toString()],
+        [web3.utils.toBN(proof.f0).toString(), web3.utils.toBN(proof.f1).toString()],
+        defaultAccount,
+        { from: defaultAccount },
+      )
+      .then(
+        (res: any) => {
+          for (let i = 0; i < res.logs.length; i++) {
+            const log = res.logs[i];
 
-    setVotes([...votes, vote]);
-    setProofVerified(yesBallot);
-
-    console.log('ballot.verifyVote verified -->', yesBallot);
+            if (log.event == 'VotingSuccessEvent' && log.args[1] === true) {
+              console.log('Voting successful');
+              break;
+            }
+          }
+        },
+        (error: any) => console.log('something went wrong: ', error),
+      );
   };
 
-  const endVoting = () => {
+  const endVoting = async () => {
+    const _votes = [];
+    const _nrVotes = await ballot.getNumberOfVotes();
+
+    for (let i = 0; i < _nrVotes.toNumber(); i++) {
+      const vote = await ballot.getVote(i);
+      const c: Cipher = {
+        a: vote[0],
+        b: vote[0],
+      };
+      _votes.push(c);
+    }
+
+    const systemParameters = await ballot.getParameters();
+    const publicKey = await ballot.getPublicKey();
+
     const params: FFelGamal.PublicKey = {
-      p: localSystemParams[0],
-      q: localSystemParams[1],
-      g: localSystemParams[2],
-      h: localPublicKey,
+      p: systemParameters[0],
+      q: systemParameters[1],
+      g: systemParameters[2],
+      h: publicKey,
     };
-    const sum = Voting.addVotes(votes, params);
+
+    const sum = Voting.addVotes(_votes, params);
     const proof = SumZKP.generateSumProof(sum, params, privateKey, defaultAccount);
+
+    // TODO: verify in contract
     const verifiedProof = SumZKP.verifySumProof(sum, proof, params, defaultAccount);
-
-    console.log('sum proof correct -> ', verifiedProof);
-  };
-
-  const allInOne = async () => {
-    const pk = Encryption.generateKeysZKP(23, 2)[0];
-    const uniqueID = defaultAccount;
-
-    // deploy contract and pass system parameters
-    await ballot.setParameters([pk.p, pk.q, pk.g], { from: defaultAccount });
-    await ballot.setPublicKey(pk.h, { from: defaultAccount });
-    await ballot.createVerifiers({ from: defaultAccount });
-
-    // yes vote
-    const yesVote = 1;
-    const yesEnc = Encryption.encrypt(yesVote, pk);
-    const yesProof = VoteZKP.generateYesProof(yesEnc, pk, uniqueID);
-
-    const yesVoteVerified = await ballot.verifyVote(
-      [web3.utils.toBN(yesEnc.a).toString(), web3.utils.toBN(yesEnc.b).toString()],
-      [web3.utils.toBN(yesProof.a0).toString(), web3.utils.toBN(yesProof.a1).toString()],
-      [web3.utils.toBN(yesProof.b0).toString(), web3.utils.toBN(yesProof.b1).toString()],
-      [web3.utils.toBN(yesProof.c0).toString(), web3.utils.toBN(yesProof.c1).toString()],
-      [web3.utils.toBN(yesProof.f0).toString(), web3.utils.toBN(yesProof.f1).toString()],
-      uniqueID,
-    );
-
-    setProofVerified(yesVoteVerified);
-
-    console.log('allInOne verified -->', yesVoteVerified);
   };
 
   return (
@@ -145,19 +124,14 @@ const PlayTrough: React.FC = () => {
         {systemParametersLoaded ? <span>OK</span> : null}
       </div>
       <div>
-        <button onClick={getSystemParams}>getSystemParams</button>
-        {localSystemParamsLoaded ? <span>OK</span> : null}
-      </div>
-      <div>
-        <button onClick={getPublicKey}>getPublicKey</button>
-        {localPublicKeyLoaded ? <span>OK</span> : null}
-      </div>
-      <div>
         <button onClick={createVerifiers}>createVerifiers</button>
         {verifiersLoaded ? <span>OK</span> : null}
       </div>
       <div>
-        <button onClick={castYesVote}>create Proof and cast Vote</button>
+        <button onClick={() => castVote(0)}>submit NO VOTE with Proof</button>
+      </div>
+      <div>
+        <button onClick={() => castVote(1)}>submit YES VOTE with Proof</button>
         {proofVerified ? (
           <span>
             <strong>Proof verified</strong>
@@ -168,10 +142,6 @@ const PlayTrough: React.FC = () => {
       </div>
       <div>
         <button onClick={endVoting}>End Voting, calculate sum and sum-proof</button>
-      </div>
-      <hr></hr>
-      <div>
-        <button onClick={allInOne}>all in one</button>
       </div>
     </div>
   );
