c0cf9a8407867bac5c8dad71e027fca096bf0c2c
nikzaugg <zauggnik@gmail.com>
2019-11-14 08:48:38 +0100

    voter frontend - add Ballot.sol and update VoteProofVerifier.sol

--- voter-frontend/contracts/Ballot.sol
+++ voter-frontend/contracts/Ballot.sol
@@ -1,10 +1,55 @@
 pragma solidity ^0.5.0;
 
+import './VoteProofVerifier.sol';
+
 contract Ballot {
 
-	uint number = 1234567890;
+	struct SystemParameters {
+    uint p; // prime
+    uint q; // prime factor: p = 2*q+1
+    uint g; // generator
+  }
 
-	function test() public view returns(uint) {
-		return number;
+	struct PublicKey {
+		uint h;
+	}
+
+	VoteProofVerifier voteVerifier;
+	SystemParameters systemParameters;
+	PublicKey publicKey;
+
+	constructor() public{
+		voteVerifier = new VoteProofVerifier();
+	}
+
+  function setParameters(uint[3] memory params) public {
+      systemParameters = SystemParameters(params[0], params[1], params[2]);
+  }
+
+  function getParameters() public view returns (uint[3] memory) {
+      return [systemParameters.p, systemParameters.q, systemParameters.g];
+  }
+
+	function setPublicKey(uint key) public {
+			publicKey = PublicKey(key);
+	}
+
+	function getPublicKey() public view returns (uint) {
+		return publicKey.h;
+	}
+
+	function createVerifiers() public {
+		voteVerifier.initialize(systemParameters.p, systemParameters.q, systemParameters.g, publicKey.h);
+	}
+
+	function verifyVote(
+		uint[2] memory cipher, // a, b
+    uint[2] memory a,
+    uint[2] memory b,
+    uint[2] memory c,
+    uint[2] memory f,
+		address id
+	) public view returns(bool) {
+		return voteVerifier.verifyProof(cipher, a, b, c, f, id);
 	}
 }
