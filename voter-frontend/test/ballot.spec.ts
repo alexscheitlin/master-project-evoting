149ec4c5dced4d5879b92d26f8f2f32c0bfe0e1d
nikzaugg <zauggnik@gmail.com>
2019-11-19 21:43:08 +0100

    solidity - adjust ballot.spec.ts to new addVotes function in /crypto

--- voter-frontend/test/ballot.spec.ts
+++ voter-frontend/test/ballot.spec.ts
@@ -2,7 +2,7 @@
 const Ballot = artifacts.require('./Ballot.sol');
 
 import { assert } from 'chai';
-import { FFelGamal, Cipher } from 'mp-crypto';
+import { FFelGamal, Cipher, SumProof } from 'mp-crypto';
 import BN from 'bn.js';
 import { toSystemParams, toParamsWithPubKey, toHex } from './helper';
 
@@ -11,6 +11,9 @@ const { KeyGeneration, Voting, VoteZKP, SumZKP } = FFelGamal;
 //@ts-ignore
 contract('Ballot.sol', () => {
   it('Ballot.sol e2e Test', async () => {
+    // ganache-cli needs to be running with `npm run ganache:dev` such that the sender account is the same (for the verifiers)
+    const address = '0x05f5e01f2d2073c8872aca4213fd85f382ca681a';
+
     /**
      * 1. SETUP BUND
      *
@@ -22,20 +25,20 @@ contract('Ballot.sol', () => {
     const q_: number = (p_ - 1) / 2;
     const g_: number = 2;
 
-    const systemWideParams: FFelGamal.SystemParameters = KeyGeneration.generateSystemParameters(p_, q_, g_);
-    await ballotContract.setParameters([systemWideParams.p, systemWideParams.q, systemWideParams.g]);
+    const bund_systemParams: FFelGamal.SystemParameters = KeyGeneration.generateSystemParameters(p_, q_, g_);
+    await ballotContract.setParameters([bund_systemParams.p, bund_systemParams.q, bund_systemParams.g]);
 
     /**
      * 2.1 SETUP AUTHORITY 1 (KANTON)
      *
      * generates and submits key shares + proof for generating the system wide public key
      */
-    const paramsInContractAuth1 = await ballotContract.getParameters();
-    const systemParamsAuth1 = toSystemParams(paramsInContractAuth1);
-    const keyShareAuth1: FFelGamal.KeyShare = KeyGeneration.generateKeyShares(systemParamsAuth1);
-    const uniqueIdAuth1 = 'IamReallyUnique;-)';
-    const keyGenProofAuth_1 = KeyGeneration.generateKeyGenerationProof(systemParamsAuth1, keyShareAuth1, uniqueIdAuth1);
-    await ballotContract.submitPublicKeyShare(keyShareAuth1.h_, keyGenProofAuth_1.d, keyGenProofAuth_1.d);
+    const auth1_sysParamsFromContract = await ballotContract.getParameters();
+    const auth1_sysParams = toSystemParams(auth1_sysParamsFromContract);
+    const auth1_keyShare: FFelGamal.KeyShare = KeyGeneration.generateKeyShares(auth1_sysParams);
+    const auth1_uniqueID = address;
+    const auth1_keyGenProof = KeyGeneration.generateKeyGenerationProof(auth1_sysParams, auth1_keyShare, auth1_uniqueID);
+    await ballotContract.submitPublicKeyShare(auth1_keyShare.h_, auth1_keyGenProof.d, auth1_keyGenProof.d);
 
     // assert the correct numbers of key shares
     let nrOfShares = await ballotContract.getSharesLength();
@@ -46,12 +49,12 @@ contract('Ballot.sol', () => {
      *
      * generates and submits key shares + proof for generating the system wide public key
      */
-    const paramsInContractAuth2 = await ballotContract.getParameters();
-    const systemParamsAuth2 = toSystemParams(paramsInContractAuth2);
-    const keyShareAuth2: FFelGamal.KeyShare = KeyGeneration.generateKeyShares(systemParamsAuth2);
-    const uniqueIdAuth2 = 'IamReallyUnique;-)';
-    const keyGenProofAuth_2 = KeyGeneration.generateKeyGenerationProof(systemParamsAuth2, keyShareAuth2, uniqueIdAuth2);
-    await ballotContract.submitPublicKeyShare(keyShareAuth2.h_, keyGenProofAuth_2.d, keyGenProofAuth_2.d);
+    const auth2_sysParamsFromContract = await ballotContract.getParameters();
+    const auth2_sysParams = toSystemParams(auth2_sysParamsFromContract);
+    const auth2_keyShare: FFelGamal.KeyShare = KeyGeneration.generateKeyShares(auth2_sysParams);
+    const auth2_uniqueID = address;
+    const auth2_keyGenProof = KeyGeneration.generateKeyGenerationProof(auth2_sysParams, auth2_keyShare, auth2_uniqueID);
+    await ballotContract.submitPublicKeyShare(auth2_keyShare.h_, auth2_keyGenProof.d, auth2_keyGenProof.d);
 
     // assert the correct numbers of key shares
     nrOfShares = await ballotContract.getSharesLength();
@@ -64,9 +67,9 @@ contract('Ballot.sol', () => {
     await ballotContract.createVerifiers();
 
     // assert the combined public key in contract is the same as if combined locally on one machine
-    const localPubKey = KeyGeneration.combinePublicKeys(systemParamsAuth1, [keyShareAuth1.h_, keyShareAuth2.h_]);
-    const contractPubKey = await ballotContract.getPublicKey();
-    assert(toHex(localPubKey) === toHex(contractPubKey), 'PublicKey does not match after combining shares');
+    const test_localPubKey = KeyGeneration.combinePublicKeys(auth1_sysParams, [auth1_keyShare.h_, auth2_keyShare.h_]);
+    const test_contractPubKey = await ballotContract.getPublicKey();
+    assert(toHex(test_localPubKey) === toHex(test_contractPubKey), 'PublicKey does not match after combining shares');
 
     // Bund opens the ballot
     await ballotContract.openBallot();
@@ -78,29 +81,31 @@ contract('Ballot.sol', () => {
      * 4.2 the voter creates a vote + proof and submits it to the contract
      */
 
+    const client_uniqueID = address;
+
     // Client/Voter queries the system parameters from the Ballot contract
-    const systemParamsClient = await ballotContract.getParameters();
+    const client_sysParamsFromContract = await ballotContract.getParameters();
 
     // Client/Voter queries the public key
-    const publicKeyClient = await ballotContract.getPublicKey();
+    const client_publicKey = await ballotContract.getPublicKey();
 
     // Client/Voter creates system params with publicKey
-    const params: FFelGamal.PublicKey = toParamsWithPubKey(systemParamsClient, publicKeyClient);
-
-    // ganache-cli needs to be running with `npm run ganache:dev` such that the sender account is the same (for the verifiers)
-    const uniqueIDClient = '0x05f5E01f2D2073C8872Aca4213fD85F382CA681A';
+    const client_sysParamsWithPubKey: FFelGamal.PublicKey = toParamsWithPubKey(
+      client_sysParamsFromContract,
+      client_publicKey,
+    );
 
     // generate and submit yesVote
-    const yesVote = Voting.generateYesVote(params);
-    const yesProof = VoteZKP.generateYesProof(yesVote, params, uniqueIDClient);
+    const yesVote = Voting.generateYesVote(client_sysParamsWithPubKey);
+    const yesProof = VoteZKP.generateYesProof(yesVote, client_sysParamsWithPubKey, client_uniqueID);
     await ballotContract.vote(
       [yesVote.a, yesVote.b],
       [yesProof.a0, yesProof.a1],
       [yesProof.b0, yesProof.b1],
       [yesProof.c0, yesProof.c1],
       [yesProof.f0, yesProof.f1],
-      uniqueIDClient,
-      { from: uniqueIDClient },
+      client_uniqueID,
+      { from: client_uniqueID },
     );
 
     // assert that contract saved the votes correctly
@@ -111,16 +116,16 @@ contract('Ballot.sol', () => {
     );
 
     // generate and submit noVote
-    const noVote = Voting.generateNoVote(params);
-    const noProof = VoteZKP.generateNoProof(noVote, params, uniqueIDClient);
+    const noVote = Voting.generateNoVote(client_sysParamsWithPubKey);
+    const noProof = VoteZKP.generateNoProof(noVote, client_sysParamsWithPubKey, client_uniqueID);
     await ballotContract.vote(
       [noVote.a, noVote.b],
       [noProof.a0, noProof.a1],
       [noProof.b0, noProof.b1],
       [noProof.c0, noProof.c1],
       [noProof.f0, noProof.f1],
-      uniqueIDClient,
-      { from: uniqueIDClient },
+      client_uniqueID,
+      { from: client_uniqueID },
     );
 
     // assert that contract saved the votes correctly
@@ -140,65 +145,53 @@ contract('Ballot.sol', () => {
     assert(isOpen === false, 'Ballot was not closed');
 
     /**
-     * 6. COUNTING OF THE VOTES BY BUND
+     * 6.1 COUNTING OF THE VOTES BY AUTHORITY 1
      *
-     * The bund queries all vote-ciphers and homomorphically adds them locally
-     * The bund then writes the resulting new cipher into the contract for everyone to see
-     * together with a proof.
+     * The Kanton queries all vote-ciphers and homomorphically adds them locally
+     * The Kanton generates decrypted share
+     * The Kanton then generates a proof for the sum
+     *
      */
-    const _votes = [];
-    const _votesCount = await ballotContract.getNumberOfVotes();
+    const auth1_votes = [];
+    const auth1_votesCount = await ballotContract.getNumberOfVotes();
 
-    for (let i = 0; i < _votesCount.toNumber(); i++) {
+    for (let i = 0; i < auth1_votesCount.toNumber(); i++) {
       const vote = await ballotContract.getVote(i);
       const c: Cipher = {
         a: vote[0],
         b: vote[1],
       };
-      _votes.push(c);
+      auth1_votes.push(c);
     }
-
-    const bundParams = await ballotContract.getParameters();
-    const bundPublicKey = await ballotContract.getPublicKey();
-    const bundParamsWithPublicKey: FFelGamal.PublicKey = toParamsWithPubKey(bundParams, bundPublicKey);
-    // FIXME: (*) cannot create a proof for this sum, as the bund does not have a private key share..
-    // but if each authority creates the sum of votes and submits a proof, then the sums will never be the same,
-    // as the counting in addVotes starts with a `generateNoVote`, and inside this function there is a
-    // random value
-    const bundSumCipher = Voting.addVotes(_votes, bundParamsWithPublicKey);
-
-    /**
-     * 7.1 AUTHORITY 1 (KANTON) decryption of the final sum
-     *
-     * gets sum cipher of votes from the Ballot contract
-     * decrypts the homomorphic encrypted sum of votes with its private key share
-     * generates a sum proof
-     * submit decrypted share + sumproof
-     */
-    // FIXME: see fixme (*) above
-    const decSumkeyShareAuth1 = KeyGeneration.decryptShare(systemParamsAuth1, bundSumCipher, keyShareAuth1.sk_);
-
-    // TODO: which proof do we generate here? A sum proof? Right now I generate a proof for the sum with
-    // the secret-key-share. It fails...because of the secret-key-share?
-    const sumProofAuth1 = SumZKP.generateSumProof(
-      bundSumCipher,
-      bundParamsWithPublicKey,
-      keyShareAuth1.sk_,
-      uniqueIDClient,
+    const auth1_sysParamsWithPubKey: FFelGamal.PublicKey = toParamsWithPubKey(
+      auth1_sysParamsFromContract,
+      auth1_keyShare.h_,
     );
-    // FIXME: This always returns false. Because of keyShareAuth1.sk_?
-    const verifSumproof = SumZKP.verifySumProof(bundSumCipher, sumProofAuth1, bundParamsWithPublicKey, uniqueIDClient);
+
+    // homomorphically add votes
+    const auth1_sumCipher = Voting.addVotes(auth1_votes, auth1_sysParamsWithPubKey);
+
+    // create decrypted share
+    const auth1_decryptedShare = KeyGeneration.decryptShare(auth1_sysParams, auth1_sumCipher, auth1_keyShare.sk_);
+
+    // create proof for homomorphic sum
+    const auth1_decryptedShareProof: SumProof = SumZKP.generateSumProof(
+      auth1_sumCipher,
+      auth1_sysParamsWithPubKey,
+      auth1_keyShare.sk_,
+      auth1_uniqueID,
+    );
 
     // submit decrypted share to the contract with a proof
     await ballotContract.submitSumShare(
-      decSumkeyShareAuth1,
-      bundSumCipher.a,
-      bundSumCipher.b,
-      sumProofAuth1.a1,
-      sumProofAuth1.b1,
-      sumProofAuth1.d,
-      sumProofAuth1.f,
-      uniqueIDClient,
+      auth1_decryptedShare,
+      auth1_sumCipher.a,
+      auth1_sumCipher.b,
+      auth1_decryptedShareProof.a1,
+      auth1_decryptedShareProof.b1,
+      auth1_decryptedShareProof.d,
+      auth1_decryptedShareProof.f,
+      auth1_uniqueID,
     );
 
     // assert correct number of shares are saved in the contract
@@ -206,38 +199,54 @@ contract('Ballot.sol', () => {
     assert(toHex(nrOfSumShares) === toHex(new BN(1)), 'there should be 1 share in the contract');
 
     /**
-     * 7.2 AUTHORITY 2 (KANTON) decryption of the final sum
+     * 6.1 COUNTING OF THE VOTES BY AUTHORITY 2
      *
-     * gets sum cipher of votes from the Ballot contract
-     * decrypts the homomorphic encrypted sum of votes with its private key share
-     * generates a sum proof
-     * submit decrypted share + sumproof
+     * The Kanton queries all vote-ciphers and homomorphically adds them locally
+     * The Kanton generates decrypted share
+     * The Kanton then generates a proof for the sum
+     *
      */
+    const auth2_votes = [];
+    const auth2_votesCount = await ballotContract.getNumberOfVotes();
 
-    const decSumShareAuth2 = KeyGeneration.decryptShare(systemParamsAuth2, bundSumCipher, keyShareAuth2.sk_);
+    for (let i = 0; i < auth2_votesCount.toNumber(); i++) {
+      const vote = await ballotContract.getVote(i);
+      const c: Cipher = {
+        a: vote[0],
+        b: vote[1],
+      };
+      auth2_votes.push(c);
+    }
 
-    // TODO: which proof do we generate here? A sum proof? Right now I generate a proof for the sum with
-    // the secret-key-share. It fails...because of the secret-key-share?
-    const sumProofAuth2 = SumZKP.generateSumProof(
-      bundSumCipher,
-      bundParamsWithPublicKey,
-      keyShareAuth2.sk_,
-      uniqueIDClient,
+    const auth2_paramsWithPublicKey: FFelGamal.PublicKey = toParamsWithPubKey(
+      auth2_sysParamsFromContract,
+      auth2_keyShare.h_,
     );
 
-    // FIXME: This always returns false. Because of keyShareAuth2.sk_?
-    const verifSumproof2 = SumZKP.verifySumProof(bundSumCipher, sumProofAuth2, bundParamsWithPublicKey, uniqueIDClient);
+    // homomorphically add votes
+    const auth2_sumCipher = Voting.addVotes(auth2_votes, auth2_paramsWithPublicKey);
+
+    // create decrypted share
+    const auth2_decryptedShare = KeyGeneration.decryptShare(auth2_sysParams, auth2_sumCipher, auth2_keyShare.sk_);
+
+    // create proof for homomorphic sum
+    const auth2_decryptedShareProof: SumProof = SumZKP.generateSumProof(
+      auth2_sumCipher,
+      auth2_paramsWithPublicKey,
+      auth2_keyShare.sk_,
+      auth2_uniqueID,
+    );
 
     // submit decrypted share to the contract with a proof
     await ballotContract.submitSumShare(
-      decSumShareAuth2,
-      bundSumCipher.a,
-      bundSumCipher.b,
-      sumProofAuth2.a1,
-      sumProofAuth2.b1,
-      sumProofAuth2.d,
-      sumProofAuth2.f,
-      uniqueIDClient,
+      auth2_decryptedShare,
+      auth2_sumCipher.a,
+      auth2_sumCipher.b,
+      auth2_decryptedShareProof.a1,
+      auth2_decryptedShareProof.b1,
+      auth2_decryptedShareProof.d,
+      auth2_decryptedShareProof.f,
+      auth2_uniqueID,
     );
 
     // assert correct number of shares are saved in the contract
@@ -245,19 +254,38 @@ contract('Ballot.sol', () => {
     assert(toHex(nrOfSumShares) === toHex(new BN(2)), 'there should be 1 share in the contract');
 
     /**
-     * 8. BUND computes the final sum
+     * 7. BUND computes the final result
      *
      * the Bund gets all decrypted shares (can be done by anyone)
      * the Bund combines the decrypted shares and finds the final tally result
      */
-    const sumShares = [];
+    const bund_votes = [];
+    const bund_votesCount = await ballotContract.getNumberOfVotes();
+
+    for (let i = 0; i < bund_votesCount.toNumber(); i++) {
+      const vote = await ballotContract.getVote(i);
+      const c: Cipher = {
+        a: vote[0],
+        b: vote[1],
+      };
+      bund_votes.push(c);
+    }
+
+    const bund_publicKey = await ballotContract.getPublicKey();
+    const bund_paramsFromContract = await ballotContract.getParameters();
+    const bund_paramsWithPublicKey: FFelGamal.PublicKey = toParamsWithPubKey(bund_paramsFromContract, bund_publicKey);
+
+    // homomorphically add votes
+    const bund_sumCipher = Voting.addVotes(bund_votes, bund_paramsWithPublicKey);
+
+    const bund_sumShares = [];
     for (let i = 0; i < nrOfSumShares.toNumber(); i++) {
       const sumShare = await ballotContract.getSumShare(i);
-      sumShares.push(sumShare);
+      bund_sumShares.push(sumShare);
     }
 
     // finish decryption by combining decrypted shares
-    const nrOfYesVotes = KeyGeneration.combineDecryptedShares(systemParamsAuth2, bundSumCipher, [...sumShares]);
+    const nrOfYesVotes = KeyGeneration.combineDecryptedShares(bund_systemParams, bund_sumCipher, [...bund_sumShares]);
     assert(toHex(nrOfYesVotes) === toHex(new BN(1)), 'wrong voting result');
   });
 });
