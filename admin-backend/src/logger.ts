c3fe2434508330666c950b626e14f66752180a99
Moritz Eck <moritz.eck@gmail.com>
2019-11-20 11:23:48 +0100

    Admin-Backend: Logger Extension

--- admin-backend/src/logger.ts
+++ admin-backend/src/logger.ts
@@ -1,7 +1,11 @@
 import express from 'express'
 
 const logger = (request: express.Request, response: express.Response, next: express.NextFunction) => {
-  console.log(`${request.method} ${request.path}`)
+  if (request.method === 'POST') {
+    console.log(`${request.method} ${request.path} ${JSON.stringify(request.body)}`)
+  } else {
+    console.log(`${request.method} ${request.path}`)
+  }
   next()
 }
 
