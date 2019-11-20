1cc087e5c581eac8fb86e784088ff7f54e4ce619
nikzaugg <zauggnik@gmail.com>
2019-11-20 11:24:39 +0100

    ballot - rename Verifier to VoteProofVerifierEC

--- voter-frontend/migrations/2_deploy_contracts.js
+++ voter-frontend/migrations/2_deploy_contracts.js
@@ -1,19 +1,20 @@
 const Ballot = artifacts.require('Ballot');
 const EllipticLib = artifacts.require('EllipticCurve');
-const Verifier = artifacts.require('Verifier');
+const VoteProofVerifierEC = artifacts.require('VoteProofVerifierEC');
 const VoteProofVerifier = artifacts.require('VoteProofVerifier');
 const SumProofVerifier = artifacts.require('SumProofVerifier');
 const ModuloMathLib = artifacts.require('ModuloMath');
 
 module.exports = function(deployer) {
+  // link the ModuloMath library
   deployer.deploy(ModuloMathLib);
   deployer.link(ModuloMathLib, [SumProofVerifier, VoteProofVerifier, Ballot]);
   deployer.deploy(SumProofVerifier);
   deployer.deploy(VoteProofVerifier);
   deployer.deploy(Ballot);
 
+  // link the Elliptic library
   deployer.deploy(EllipticLib);
-  deployer.link(EllipticLib, Verifier);
-
-  deployer.deploy(Verifier);
+  deployer.link(EllipticLib, VoteProofVerifierEC);
+  deployer.deploy(VoteProofVerifierEC);
 };
