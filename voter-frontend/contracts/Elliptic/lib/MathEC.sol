0eefbd4e3aca9a02daa4d92de7f6f439e9c04ed9
nikzaugg <zauggnik@gmail.com>
2019-11-27 17:57:17 +0100

    voter frontend - adjust MathEC library to curve25519-weier

--- voter-frontend/contracts/Elliptic/lib/MathEC.sol
+++ voter-frontend/contracts/Elliptic/lib/MathEC.sol
@@ -1,15 +1,16 @@
 pragma solidity ^0.5.13;
 
-import "./EllipticCurveLib.sol";
+import './EllipticCurveLib.sol';
 
 // Elliptic Curve math functions used by the contracts in the eVoting system
 library MathEC {
-    // TODO: adjust to new curve
-    uint256 constant GX = 0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798; // generator
-    uint256 constant GY = 0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8; // generator
-    uint256 constant PP = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F; // modulus of curve
-    uint256 constant AA = 0;
-    uint256 constant BB = 7; // constant of curve
+    // curve25519-weier
+    uint256 constant GX = 0x2aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaad245a; // generator
+    uint256 constant GY = 0x20ae19a1b8a086b4e01edd2c7748d14c923d4d7e6d7c61b229e9c5a27eced3d9; // generator
+    uint256 constant PP = 0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffed; // modulus of curve
+    uint256 constant AA = 0x2aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa984914a144;
+    uint256 constant BB = 0x7b425ed097b425ed097b425ed097b425ed097b425ed097b4260b5e9c7710c864; // constant of curve
+    uint256 constant NN = 0x1000000000000000000000000000000014def9dea2f79cd65812631a5cf5d3ed; // for BN mod
 
     function gx() public pure returns (uint256) {
         return GX;
@@ -21,6 +22,10 @@ library MathEC {
 
     function curveModulus() public pure returns (uint256) {
         return PP;
+    }
+
+    function n() public pure returns (uint256) {
+        return NN;
     }
 
     function isOnCurve(uint256 x_1, uint256 y_1) public pure returns (bool) {
