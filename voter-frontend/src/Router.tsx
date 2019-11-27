a83270c19c16fecee707a3652f4a9df15af49383
nikzaugg <zauggnik@gmail.com>
2019-11-27 22:23:00 +0100

    voter frontend - add pages - Public - Login - Voting

--- voter-frontend/src/Router.tsx
+++ voter-frontend/src/Router.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 import {Route, Switch} from 'react-router-dom';
 import Demo from './components/Demo';
-import LoginPage from './views/LoginPage';
+import LoginPage from './pages/LoginPage';
+import VotingPage from './pages/VotingPage';
+import PublicPage from './pages/PublicPage';
 
 export const Routes: React.FC = () => {
   return (
     <Switch>
-      <Route exact path="/" component={LoginPage} />
+      <Route path="/" component={PublicPage} />
+      <Route path="/login" component={LoginPage} />
+      <Route path="/voting" component={VotingPage} />
       <Route path="/demo" component={Demo} />
     </Switch>
   );
