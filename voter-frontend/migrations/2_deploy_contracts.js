216c5ce112d462e1c84582c1223536b5690ffa90
nikzaugg <zauggnik@gmail.com>
2019-11-27 11:56:43 +0100

    voter frontend - deployment of contracts - link MathEC.sol - update solc version

--- voter-frontend/migrations/2_deploy_contracts.js
+++ voter-frontend/migrations/2_deploy_contracts.js
@@ -1,11 +1,11 @@
-const Ballot = artifacts.require('Ballot');
-const EllipticCurveLib = artifacts.require('EllipticCurveLib');
-const MathEC = artifacts.require('MathEC');
-const VoteProofVerifierEC = artifacts.require('VoteProofVerifierEC');
-const VoteProofVerifier = artifacts.require('VoteProofVerifier');
-const SumProofVerifier = artifacts.require('SumProofVerifier');
-const ModuloMathLib = artifacts.require('ModuloMathLib');
-const KeyGenProofVerifier = artifacts.require('KeyGenProofVerifier');
+const Ballot = artifacts.require("Ballot");
+const EllipticCurveLib = artifacts.require("EllipticCurveLib");
+const MathEC = artifacts.require("MathEC");
+const VoteProofVerifierEC = artifacts.require("VoteProofVerifierEC");
+const VoteProofVerifier = artifacts.require("VoteProofVerifier");
+const SumProofVerifier = artifacts.require("SumProofVerifier");
+const ModuloMathLib = artifacts.require("ModuloMathLib");
+const KeyGenProofVerifier = artifacts.require("KeyGenProofVerifier");
 
 module.exports = function(deployer) {
   // link the ModuloMath library
@@ -18,8 +18,9 @@ module.exports = function(deployer) {
 
   // link the Elliptic library
   deployer.deploy(EllipticCurveLib);
-  deployer.deploy(MathEC);
   deployer.link(EllipticCurveLib, MathEC);
+  deployer.deploy(MathEC);
+  deployer.link(MathEC, EllipticCurveLib);
   deployer.link(MathEC, [VoteProofVerifierEC]);
   deployer.deploy(VoteProofVerifierEC);
 };
