409669d74da8a58eae81d2c1c5a54a688f5a8c11
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2019-11-28 14:16:51 +0100

    identity-provider-backend - add dummy identities

--- identity-provider-backend/src/database/database.ts
+++ identity-provider-backend/src/database/database.ts
@@ -3,6 +3,7 @@ import { AdapterSync } from 'lowdb'
 import low from 'lowdb'
 import fs from 'fs'
 import FileSync from 'lowdb/adapters/FileSync'
+import { Identity } from '../models'
 
 // TODO: replace with correct type
 let db: any
@@ -11,9 +12,23 @@ export const setupDB = () => {
   const adapter: AdapterSync = new FileSync('./src/database/db.json')
   db = low(adapter)
 
+  // read identities and map them to the Identity interface
+  // assumption: voters already have an existing eIdentity
+  // uuids version 4 generated with: https://www.uuidgenerator.net/
+  const rawIdentities = fs.readFileSync('./src/database/identities', 'utf-8')
+  const identities = []
+  for (const identity of rawIdentities.split('\n')) {
+    const values = identity.split(':')
+    identities.push(<Identity>{
+      uuid: values[0],
+      username: values[1],
+      password: values[2],
+    })
+  }
+
   // set defaults (if JSON is empty)
   db.defaults({
-    identities: [],
+    identities: identities,
     voters: [],
     tokens: []
   }).write()
---
+++ identity-provider-backend/src/models.ts
@@ -0,0 +1,5 @@
+export interface Identity {
+    uuid: string;
+    username: string;
+    password: string;
+}
