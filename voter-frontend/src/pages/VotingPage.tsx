dcc93e9b20880727bcbdcb55b5e435b4e5e5e3eb
nikzaugg <zauggnik@gmail.com>
2019-11-28 16:07:32 +0100

    voter frontend - add Question Component in VoterPage

--- voter-frontend/src/pages/VotingPage.tsx
+++ voter-frontend/src/pages/VotingPage.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
+import {Grid} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles';
+import Question from '../components/Question/Question';
+
+const useStyles = makeStyles(theme => ({
+  question: {
+    height: '30%',
+    border: '1px solid red',
+  },
+  chain: {
+    height: '20%',
+    border: '1px solid blue',
+  },
+  voting: {
+    height: '50%',
+    border: '1px solid green',
+  },
+}));
 
 const VotingPage: React.FC = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>Voting Page</h1>
-      <button>Yes</button>
-      <button>No</button>
-    </div>
+    <Grid container direction="column" style={{height: '100%'}}>
+      <Grid item className={classes.question}>
+        <Question />
+      </Grid>
+      <Grid item className={classes.chain}>
+        Chain Information
+      </Grid>
+      <Grid item className={classes.voting}>
+        Voting Panel
+      </Grid>
+    </Grid>
   );
 };
 
