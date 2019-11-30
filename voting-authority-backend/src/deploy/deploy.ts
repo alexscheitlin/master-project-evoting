28ea571ee714a37c03d9d1812de79dc7d5baff05
Moritz Eck <moritz.eck@gmail.com>
2019-11-30 11:12:14 +0100

    Voting-Authority-Backend
    - remove remaining solidity/compile parts
    - modifiy DB to allow for boolean values from /deploy route

--- voting-authority-backend/src/deploy/deploy.ts
+++ voting-authority-backend/src/deploy/deploy.ts
@@ -1,6 +1,6 @@
 import express from 'express'
-import { setValue, getValueFromDB } from '../database/database'
 import * as Deploy from '../../solidity/scripts/deploy'
+import { getValueFromDB, setValue } from '../database/database'
 
 const BALLOT_DEPLOYED_SUCCESS_MESSAGE: string = 'Ballot successfully deployed.'
 const BALLOT_ALREADY_DEPLOYED_MESSAGE: string = 'Ballot already deployed.'
@@ -12,18 +12,18 @@ const DEPLOYMENT_STATE: string = 'ballotDeployed'
 const router: express.Router = express.Router()
 
 router.post('/deploy', (req, res) => {
-  const isDeployed: string = getValueFromDB(DEPLOYMENT_STATE)
+  const isDeployed: boolean = <boolean>getValueFromDB(DEPLOYMENT_STATE)
 
-  if (isDeployed === 'true') {
-    const address: string = getValueFromDB(DEPLOYMENT_ADDRESS)
+  if (isDeployed) {
+    const address: boolean = <boolean>getValueFromDB(DEPLOYMENT_ADDRESS)
     res.status(201).json({ address: address, msg: BALLOT_ALREADY_DEPLOYED_MESSAGE })
   }
 
-  if (isDeployed === 'false') {
+  if (isDeployed === false) {
     Deploy.init()
       .then(addr => {
         setValue(DEPLOYMENT_ADDRESS, addr)
-        setValue(DEPLOYMENT_STATE, 'true')
+        setValue(DEPLOYMENT_STATE, true)
         res.status(201).json({ address: addr, msg: BALLOT_DEPLOYED_SUCCESS_MESSAGE })
       })
       .catch(err => {
@@ -33,11 +33,11 @@ router.post('/deploy', (req, res) => {
 })
 
 router.get('/deploy', (req, res) => {
-  const isDeployed: string = getValueFromDB(DEPLOYMENT_STATE)
+  const isDeployed: boolean = <boolean>getValueFromDB(DEPLOYMENT_STATE)
 
-  if (isDeployed === 'true') {
-    const address: string = getValueFromDB(DEPLOYMENT_ADDRESS)
-    res.status(200).json({ address: address })
+  if (isDeployed) {
+    const address: boolean = <boolean>getValueFromDB(DEPLOYMENT_ADDRESS)
+    res.status(200).json({ address: address, msg: BALLOT_ALREADY_DEPLOYED_MESSAGE })
   } else {
     res.status(400).json({ msg: BALLOT_NOT_MESSAGE })
   }
