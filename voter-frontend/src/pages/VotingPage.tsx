1781347f41bc98dec2d38e0f316af528333df049
nikzaugg <zauggnik@gmail.com>
2019-12-01 00:15:15 +0100

    voter frontend - display chain and wallet information in VotingPanel.tsx

--- voter-frontend/src/pages/VotingPage.tsx
+++ voter-frontend/src/pages/VotingPage.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Grid} from '@material-ui/core';
 import {makeStyles} from '@material-ui/core/styles';
 import Question from '../components/Question/Question';
+import getWeb3 from '../util/getWeb3';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles(() => ({
   question: {
     height: '30%',
     border: '1px solid red',
@@ -20,13 +21,42 @@ const useStyles = makeStyles(theme => ({
 
 const VotingPage: React.FC = () => {
   const classes = useStyles();
+  const [balance, setBalance] = useState('');
+  const [walletAddress, setWalletAddress] = useState('');
+
+  const getChainInfo = async (): Promise<void> => {
+    const web3 = await getWeb3();
+
+    const defaultAcc = web3.eth.defaultAccount;
+    if (defaultAcc !== null) {
+      setWalletAddress(defaultAcc);
+      const balance = await web3.eth.getBalance(defaultAcc);
+      setBalance(balance);
+    }
+  };
+
+  useEffect(() => {
+    getChainInfo();
+  }, []);
+
   return (
     <Grid container direction="column" style={{height: '100%'}}>
       <Grid item className={classes.question}>
         <Question />
       </Grid>
       <Grid item className={classes.chain}>
-        Chain Information
+        <div>
+          <strong>Contract Address: </strong>
+          <span>{localStorage.getItem('address')}</span>
+        </div>
+        <div>
+          <strong>My Wallet Address: </strong>
+          <span>{walletAddress}</span>
+        </div>
+        <div>
+          <strong>My Wallet Balance: </strong>
+          <span>{balance}</span>
+        </div>
       </Grid>
       <Grid item className={classes.voting}>
         Voting Panel
