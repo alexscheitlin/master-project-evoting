27ea8151de43714cfbb7b6703b1215ea1b18e246
nikzaugg <zauggnik@gmail.com>
2019-12-02 16:21:17 +0100

    access backend - get chain url from .env file

--- access-provider-backend/src/utils/web3.ts
+++ access-provider-backend/src/utils/web3.ts
@@ -1,9 +1,7 @@
 import Web3 from 'web3'
 
 export const getWeb3 = (): Web3 => {
-  // TODO: replace dynamically with real address of network
-  const provider = new Web3.providers.HttpProvider('http://localhost:8545')
+  const provider = new Web3.providers.HttpProvider(process.env.CHAIN_URL as string)
   const web3: Web3 = new Web3(provider)
-
   return web3
 }
