6b8cee425c5d54fe0f2374d4daf255a91d7db1bd
nikzaugg <zauggnik@gmail.com>
2019-12-02 09:21:55 +0100

    voter frontend - get voting question from backend

--- voter-frontend/src/pages/VotingPage.tsx
+++ voter-frontend/src/pages/VotingPage.tsx
@@ -1,6 +1,7 @@
-import React, {useEffect, useState} from 'react';
-import {Grid} from '@material-ui/core';
-import {makeStyles} from '@material-ui/core/styles';
+import React, { useEffect, useState } from 'react';
+import BallotContract from '../contract-abis/Ballot.json';
+import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import Question from '../components/Question/Question';
 import getWeb3 from '../util/getWeb3';
 
@@ -23,31 +24,40 @@ const VotingPage: React.FC = () => {
   const classes = useStyles();
   const [balance, setBalance] = useState('');
   const [walletAddress, setWalletAddress] = useState('');
+  const [contractAddress, setContractAddress] = useState('');
+  const [votingQuestion, setVotingQuestion] = useState('');
 
   const getChainInfo = async (): Promise<void> => {
     const web3 = await getWeb3();
-
-    const defaultAcc = web3.eth.defaultAccount;
-    if (defaultAcc !== null) {
-      setWalletAddress(defaultAcc);
-      const balance = await web3.eth.getBalance(defaultAcc);
-      setBalance(balance);
+    const contractAddr = localStorage.getItem('address');
+    if (contractAddr !== null) {
+      setContractAddress(contractAddr);
+      const defaultAcc = web3.eth.defaultAccount;
+      if (defaultAcc !== null) {
+        setWalletAddress(defaultAcc);
+        const balance = await web3.eth.getBalance(defaultAcc);
+        setBalance(balance);
+        const contract = new web3.eth.Contract(BallotContract.abi, contractAddr);
+        const question = await contract.methods.getVotingQuestion().call({ from: walletAddress });
+        setVotingQuestion(question);
+      }
     }
   };
 
   useEffect(() => {
     getChainInfo();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <Grid container direction="column" style={{height: '100%'}}>
+    <Grid container direction="column" style={{ height: '100%' }}>
       <Grid item className={classes.question}>
-        <Question />
+        <Question votingQuestion={votingQuestion} />
       </Grid>
       <Grid item className={classes.chain}>
         <div>
           <strong>Contract Address: </strong>
-          <span>{localStorage.getItem('address')}</span>
+          <span>{contractAddress}</span>
         </div>
         <div>
           <strong>My Wallet Address: </strong>
