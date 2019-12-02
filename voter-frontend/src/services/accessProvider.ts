d864becc43c71315a4ede050339e02d878b1d32c
nikzaugg <zauggnik@gmail.com>
2019-12-02 16:29:39 +0100

    voter frontend - remove mock accessProvider - the funding now happens serverside

--- voter-frontend/src/services/accessProvider.ts
+++ voter-frontend/src/services/accessProvider.ts
@@ -1,39 +1,17 @@
-import getWeb3 from '../util/getWeb3';
+/* eslint-disable no-undef */
 import axios from 'axios';
+
 import { config } from '../config';
-import {
-  NO_ACCOUNTS_FOUND_ERROR_MESSAGE,
-  NOT_FUNDED_ERROR_MESSAGE,
-  NO_CONTRACT_ADDRESS_ERROR_MESSAGE,
-} from '../constants';
+
+const ACCESS_PROVIDER_URL =
+  process.env.NODE_ENV === 'development' ? config.accessProviderUrl.dev : config.accessProviderUrl.prod;
 
 export const fundWallet = async (token: string, wallet: string): Promise<string> => {
-  // TODO: the following code should be happening on the accessProvider
-  // The Access Provider should get the address from the client and send some ether to it
-  // In the return, it should also return the contract-address
+  const requestBody = {
+    token: token,
+    address: wallet,
+  };
 
-  let accounts: string[];
-
-  const web3 = await getWeb3();
-  try {
-    accounts = await web3.eth.getAccounts();
-  } catch (error) {
-    throw new Error(NO_ACCOUNTS_FOUND_ERROR_MESSAGE);
-  }
-
-  const receiver = web3.eth.defaultAccount;
-  if (receiver !== null) {
-    try {
-      await web3.eth.sendTransaction({ from: accounts[0], to: receiver, value: web3.utils.toWei('1', 'ether') });
-    } catch (error) {
-      throw new Error(NOT_FUNDED_ERROR_MESSAGE);
-    }
-  }
-  try {
-    // TODO: the access provider should return this address
-    const address = await axios.get(config.backend + '/deploy');
-    return address.data.address;
-  } catch (error) {
-    throw new Error(NO_CONTRACT_ADDRESS_ERROR_MESSAGE);
-  }
+  const res = await axios.post(ACCESS_PROVIDER_URL + '/register', requestBody);
+  return res.data.ballot;
 };
