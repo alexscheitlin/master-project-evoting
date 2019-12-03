3314832f97e320bddfb2885a8f2a4adce1efc33c
nikzaugg <zauggnik@gmail.com>
2019-12-03 10:07:01 +0100

    voter frontend - add getToken request for identity provider

--- voter-frontend/src/services/eIdentityProvider.ts
+++ voter-frontend/src/services/eIdentityProvider.ts
@@ -1,4 +1,21 @@
-export const getToken = async (username: string, password: string) => {
-  // TODO: login with the identity provider and return the retrieved token on success
-  return '1234';
+/* eslint-disable no-undef */
+import axios from 'axios';
+
+import { config } from '../config';
+
+const IDENTITY_PROVIDER_URL =
+  process.env.NODE_ENV === 'development' ? config.identityProviderUrl.dev : config.identityProviderUrl.prod;
+
+export const getToken = async (username: string, password: string): Promise<string> => {
+  const requestBody = {
+    username: username,
+    password: password,
+  };
+
+  try {
+    const res = await axios.post(IDENTITY_PROVIDER_URL + '/getToken', requestBody);
+    return res.data.token;
+  } catch (error) {
+    throw new Error('Login unsuccessful');
+  }
 };
