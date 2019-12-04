027445f8776329a9bbc7fe11b07c2d4f116280ef
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2019-12-04 16:49:56 +0100

    contracts - specify the number of authority nodes in ballot constructor

--- contracts/test/finite-field/ballot.spec.ts
+++ contracts/test/finite-field/ballot.spec.ts
@@ -17,7 +17,7 @@ contract('Ballot.sol', () => {
      * Bund initialized the whole voting process by creating the system parameters, deploying the contract
      * and setting the parameters int the contract
      */
-    const ballotContract = await Ballot.new('Is the dress blue or gold?');
+    const ballotContract = await Ballot.new('Is the dress blue or gold?', 2);
     const votingQuestion = await ballotContract.getVotingQuestion();
     assert(votingQuestion === 'Is the dress blue or gold?', 'voting question not correct');
 
