5742183fdf23e57d23b3e5c50a8565afe8689918
nikzaugg <zauggnik@gmail.com>
2019-12-05 14:25:26 +0100

    sealer node - added dummy routes /register and /boot

---
+++ sealer-node/backend/src/routes/register.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+
+import { runScript } from '../utils/shell'
+
+const router: express.Router = express.Router()
+
+// messages
+const WALLET_REGISTERED_MSG: string = 'Wallet successfully registered!'
+
+router.post('/register', async (req, res) => {
+  const { stdout, stderr } = await runScript('src/scripts/test.sh')
+
+  const addressToRegister = req.body.address
+  console.log('...will register:', addressToRegister)
+
+  // Read public key created (keyStore)
+
+  // Send public key to authority backend to register
+
+  // subscribe to changes (as soon as chainspec is ready, the node can be started)
+
+  res.status(200).json({ msg: WALLET_REGISTERED_MSG })
+})
+
+export default router
