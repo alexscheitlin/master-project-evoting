e1e386a9d6d110e245d92b93fd571a90e8e399f9
nikzaugg <zauggnik@gmail.com>
2019-12-08 23:11:16 +0100

    react frontend - add logic and component for second step of the sealer-process

--- sealer/frontend/src/services/sealerBackend.ts
+++ sealer/frontend/src/services/sealerBackend.ts
@@ -11,3 +11,29 @@ export const registerWallet = async (wallet: string) => {
   const url = "http://localhost:" + process.env.REACT_APP_BACKEND_PORT;
   await axios.post(url + "/register");
 };
+
+export const loadConfiguration = async () => {
+  const url = "http://localhost:" + process.env.REACT_APP_BACKEND_PORT;
+  const response = await axios.get(url + "/chainspec");
+  if (response.status === 200) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const findPeers = async () => {
+  const url = "http://localhost:" + process.env.REACT_APP_BACKEND_PORT;
+  const response = await axios.post(url + "/peer");
+  if (response.status === 200) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const getNrPeers = async (): Promise<number> => {
+  const url = "http://localhost:" + process.env.REACT_APP_BACKEND_PORT;
+  const response = await axios.get(url + "/peer");
+  return response.data.nrOfPeers;
+};
