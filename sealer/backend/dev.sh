359e3b5b805b763515c376df47c203675117d75b
nikzaugg <zauggnik@gmail.com>
2019-12-08 14:33:31 +0100

    sealer backend - add script that starts backend on port 4011

---
+++ sealer/backend/dev.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# dev setup
+
+sealerNr=1
+
+rm .env
+
+# copy keys
+cp ../../poa-blockchain/keys/sealer$sealerNr.json ./wallet/sealer.json
+cp ../../poa-blockchain/keys/sealer$sealerNr.pwd ./wallet/sealer.pwd
+
+# create env file for backend where to find chain etc.
+echo NODE_ENV=development >> .env
+echo REGISTRATION_NODE_URL=7010 >> .env
+echo SEALER_NODE_URL=701$sealerNr >>  .env
+echo BACKEND_PORT=401$sealerNr >>  .env
+echo FRONTEND_PORT=301$sealerNr >>  .env
+
+# start docker containers
+npm run serve:dev
