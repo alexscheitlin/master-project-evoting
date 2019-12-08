eb38af0258c194ea4e8797d6335e3e48fc22b2b8
nikzaugg <zauggnik@gmail.com>
2019-12-08 23:08:44 +0100

    auth backend - update config, chain nodes run on ports 7010 - 701x

--- voting-authority-backend/src/config.ts
+++ voting-authority-backend/src/config.ts
@@ -5,7 +5,7 @@ export const parityConfig = {
   connectionNodeUrl: 'http://localhost:8540',
 
   // node to connect to and account to deploy the contracts and interact with them
-  nodeUrl: 'http://localhost:8540',
+  nodeUrl: 'http://localhost:7010',
   accountAddress: '0x004ec07d2329997267ec62b4166639513386f32e',
   accountPassword: 'user',
 }
