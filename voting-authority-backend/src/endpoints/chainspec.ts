3401366b706ae8e5ffb0efee6faab74c892c7ff2
Moritz Eck <i22457755+meck93@users.noreply.github.com>
2019-12-08 11:05:03 +0100

    Voting-Authority-Backend
    - add: /registered endpoint for registering clients
    (voting-authority-frontends) to receive signed-up/registered
    validators/sealers (as server-sent-events).

--- voting-authority-backend/src/endpoints/chainspec.ts
+++ voting-authority-backend/src/endpoints/chainspec.ts
@@ -1,22 +1,69 @@
 import express from 'express'
-
-import { verifyAddress } from '../utils/addressVerification'
 import {
-  getValueFromDB,
-  getObjectFromDB,
-  setValue,
   addToList,
-  STATE_TABLE,
+  AUTHORITIES_TABLE,
   CHAINSPEC_TABLE,
   DEFAULT_CHAINSPEC_TABLE,
-  AUTHORITIES_TABLE,
+  getObjectFromDB,
+  getValueFromDB,
+  setValue,
+  STATE_TABLE,
 } from '../database/database'
+import { verifyAddress } from '../utils/addressVerification'
 
 const SUCCESS_MSG: string = 'Successfully registered authority address.'
 const ADDRESS_INVALID: string = 'Address registration failed. Address is not valid or has already been registered.'
 const AUTHORITY_REGISTRATION_CLOSED: string = 'Authority registration is closed. Cannot register Authority address.'
 
+// database tables
+const CHAINSPEC: string = 'chainspec'
+
+let clients: RegisteredClient[] = []
+
 const router: express.Router = express.Router()
+
+interface RegisteredClient {
+  id: number
+  res: express.Response
+}
+
+router.get('/registered', (req: express.Request, res: express.Response) => {
+  // Mandatory headers and http status to keep connection open
+  const headers = {
+    'Content-Type': 'text/event-stream',
+    Connection: 'keep-alive',
+    'Cache-Control': 'no-cache',
+  }
+  res.writeHead(200, headers)
+
+  // Get existing validators from DB
+  const chainspec: any = getObjectFromDB(CHAINSPEC)
+  const validators: string[] = chainspec['engine']['authorityRound']['params']['validators']['list']
+
+  // After client opens connection send all nests as string
+  const data = `data: ${JSON.stringify(validators)}\n\n`
+  res.write(data)
+
+  // Generate an id based on timestamp and save res
+  // object of client connection on clients list
+  // Later we'll iterate it and send updates to each client
+  const clientId = Date.now()
+  const newClient: RegisteredClient = {
+    id: clientId,
+    res,
+  }
+  clients.push(newClient)
+
+  // When client closes connection we update the clients list avoiding the disconnected one
+  req.on('close', () => {
+    console.log(`${clientId} Connection closed`)
+    clients = clients.filter(c => c.id !== clientId)
+  })
+})
+
+const sendValidatorToAllClients = (newValidator: string) => {
+  clients.forEach(c => c.res.write(`data: ${JSON.stringify([newValidator])}\n\n`))
+}
 
 router.get('/chainspec', (req, res) => {
   const state: string = <string>getValueFromDB(STATE_TABLE)
@@ -56,8 +103,12 @@ router.post('/chainspec', (req, res) => {
   addToList(AUTHORITIES_TABLE, [voterAddress])
 
   try {
+    // create the new chainspec
     const newChainspec: any = addValidatorToChainspec(oldChainspec, voterAddress)
-    setValue(CHAINSPEC_TABLE, newChainspec)
+    setValue(CHAINSPEC, newChainspec)
+
+    // update all registered clients -> send the new validator
+    sendValidatorToAllClients(voterAddress)
     res.status(201).json({ created: true, msg: SUCCESS_MSG })
   } catch (error) {
     res.status(400).json({ created: false, msg: error.message })
@@ -82,7 +133,7 @@ export const addValidatorToChainspec = (chainspec: any, address: string): any =>
   if (accounts === null || typeof accounts === undefined) {
     throw new TypeError('Accounts cannot be retrieved from chainspec since it is null.')
   }
-  accounts[`${address}`] = { balance: "10000000000000000000000" }
+  accounts[`${address}`] = { balance: '10000000000000000000000' }
   chainspec['accounts'] = accounts
 
   return chainspec
