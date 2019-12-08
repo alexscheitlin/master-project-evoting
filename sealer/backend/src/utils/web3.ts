13cea939b9a7a9a9c7ba1fca7f11da80a2d32436
nikzaugg <zauggnik@gmail.com>
2019-12-08 23:07:05 +0100

    sealer backend - add GET /peer to get nr of connected peers

--- sealer/backend/src/utils/web3.ts
+++ sealer/backend/src/utils/web3.ts
@@ -1,7 +1,9 @@
 import Web3 from 'web3'
 
 export const getWeb3 = (): Web3 => {
-  const provider = new Web3.providers.HttpProvider(process.env.CHAIN_URL as string)
+  const port = process.env.SEALER_NODE_URL as string
+  const url = 'http://localhost:' + port
+  const provider = new Web3.providers.HttpProvider(url)
   const web3: Web3 = new Web3(provider)
   return web3
 }
