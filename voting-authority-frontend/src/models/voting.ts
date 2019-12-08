e2257d41427c93da7f587d54dd630212312b430f
Moritz Eck <i22457755+meck93@users.noreply.github.com>
2019-12-08 13:00:25 +0100

    Voting-Authority-Frontend
    - add: 1. approach to update state async using zustand and axios ->
    triggered request and only update if BE has updated ->
    works but error handling needs to be improved.

--- voting-authority-frontend/src/models/voting.ts
+++ voting-authority-frontend/src/models/voting.ts
@@ -1,4 +1,7 @@
 import { create } from 'zustand';
+import axios, { AxiosResponse } from 'axios';
+import https from 'https';
+import { DEV_URL } from '../constants';
 
 export enum VotingState {
   REGISTER = 'REGISTER',
@@ -34,10 +37,36 @@ export const VOTE_LABELS: string[] = [
 
 export const [useVoteStateStore] = create(set => ({
   state: VotingState.REGISTER,
-  nextState: () =>
-    set(prevState => ({ state: VOTE_STATES[(VOTE_STATES.indexOf(prevState.state) + 1) % VOTE_STATES.length] })),
+  nextState: async () => {
+    try {
+      // avoids ssl error with certificate
+      const agent = new https.Agent({
+        rejectUnauthorized: false
+      });
+
+      // there is no error handling here on purpose -> handle in calling component
+      const response: AxiosResponse<StateResponse> = await axios.post(`${DEV_URL}/state`, {}, { httpsAgent: agent });
+      if (response.status === 201) {
+        const newState: VotingState = response.data.state;
+        set({ state: newState });
+      } else {
+        // TODO: think of a way to improve the error handling if the request fails. The goal is that zustand can be used to update the state using a request to the voting-authority-backend. The problem is that we only want to update the state in the here if the BE has update it. Therefore, we need to wait for a successful state change request. What shall we do if the request is not successful? Wait, display ErrorSnackbar, and let the user re-try.
+        throw new Error(`State cannot be updated. ${response.status}, ${JSON.stringify(response.data)}`);
+      }
+    } catch (error) {
+      console.log('inside voting.ts, error', error);
+      // TODO: rethrow the error -> higher level component should handle it. We don't want to handle it here. But maybe we could return a Promise instead? Or how do we enusre that we can catch it higher up?
+      throw new Error(error);
+    }
+  },
   reset: () => set({ state: VotingState.REGISTER })
 }));
+
+interface StateResponse {
+  state: VotingState;
+  registeredSealers: number;
+  requiredSealers: number;
+}
 
 export const [useVoteQuestionStore] = create(set => ({
   question: '',
