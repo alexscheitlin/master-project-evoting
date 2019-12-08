0ff53ca695cf05ee3311e4588b9bc291ca8011c7
nikzaugg <zauggnik@gmail.com>
2019-12-08 14:40:46 +0100

    sealer backend - add new routes - /chainspec will fetch the chainspec and write it to a file - /peer will trigger an RPC call to the running chain to register node - updated /register, will read wallet address from keyStore file and send it to auth backend

---
+++ sealer/backend/src/routes/chainspec.ts
@@ -0,0 +1,26 @@
+import axios from 'axios'
+import express from 'express'
+import fs from 'fs'
+
+import { config } from '../config'
+
+const router: express.Router = express.Router()
+// messages
+const SUCESS_MSG: string = 'Chainspec successfully fetched and written to file!'
+const SUCESS_FAIL: string = 'Chainspec could not be fetched! It might not be ready yet.'
+
+router.get('/chainspec', async (req, res) => {
+  // fetch chainspec from auth backend
+  try {
+    const result = await axios.get(config.authBackend.devUrl + '/chainspec')
+    const chainspec = JSON.stringify(result.data)
+    fs.writeFileSync('src/chainspec/chain.json', chainspec)
+
+    res.status(200).json({ msg: SUCESS_MSG })
+  } catch (error) {
+    console.log(error)
+    res.status(400).json({ msg: SUCESS_FAIL })
+  }
+})
+
+export default router
--- sealer/backend/src/server.ts
+++ sealer/backend/src/server.ts
@@ -1,6 +1,4 @@
 import express from 'express'
-import fs from 'fs'
-import helmet from 'helmet'
 import cors from 'cors'
 
 import { config } from 'dotenv'
@@ -9,6 +7,8 @@ import logger from './utils/logger'
 import { setupDB } from './database/database'
 import register from './routes/register'
 import generateKeys from './routes/generateKeys'
+import chainspec from './routes/chainspec'
+import peer from './routes/peer'
 
 config()
 
@@ -22,13 +22,16 @@ server.use(cors({ origin: 'http://localhost:' + process.env.FRONTEND_PORT }))
 // add all routes
 server.use('/', register)
 server.use('/', generateKeys)
+server.use('/', chainspec)
+server.use('/', peer)
 
 // setup the database
 setupDB()
 
 console.log(process.env.FRONTEND_PORT)
 
-server.listen(4000, () => {
-  console.log(`HTTP server started at http://localhost:${4000}`)
-  console.log(`The chain can be reached via: ${process.env.CHAIN_URL} if it's running`)
+server.listen(process.env.BACKEND_PORT, () => {
+  console.log(`HTTP server started at http://localhost:${process.env.BACKEND_PORT}`)
+  console.log(`enode registration RPC calls will be sent to ${process.env.REGISTRATION_NODE_URL}`)
+  console.log(`This parity node will be running on ${process.env.SEALER_NODE_URL}`)
 })
