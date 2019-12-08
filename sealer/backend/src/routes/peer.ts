a81c488246eec42a569e9be031f29e12db8b9d4b
nikzaugg <zauggnik@gmail.com>
2019-12-08 14:40:46 +0100

    sealer backend - add new routes - /chainspec will fetch the chainspec and write it to a file - /peer will trigger an RPC call to the running chain to register node - updated /register, will read wallet address from keyStore file and send it to auth backend

---
+++ sealer/backend/src/routes/peer.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+
+import { RPC } from '../utils'
+
+const router: express.Router = express.Router()
+
+const PEER_SUCCESS_MSG: string = 'Successfully peered with network'
+const PEER_FAIL_MSG: string = 'Successfully peered with network'
+
+router.post('/peer', async (req, res) => {
+  try {
+    const enode = await RPC.getEnodeAtPort(process.env.SEALER_NODE_URL as string)
+    await RPC.registerEnodeWithAuthority(enode)
+    res.status(200).json({ msg: PEER_SUCCESS_MSG })
+  } catch (error) {
+    console.error(error)
+    res.status(400).json({ msg: PEER_FAIL_MSG })
+  }
+})
+
+export default router
--- sealer/backend/src/routes/register.ts
+++ sealer/backend/src/routes/register.ts
@@ -1,22 +1,32 @@
+import axios from 'axios'
 import express from 'express'
+import fs from 'fs'
+import path from 'path'
+
+import { config } from '../config'
+
 const router: express.Router = express.Router()
 
 // messages
 const WALLET_REGISTERED_MSG: string = 'Wallet successfully registered!'
+const WALLET_NOT_REGISTERED_MSG: string = 'Wallet not registed with authority.'
 
 router.post('/register', async (req, res) => {
-  const addressToRegister = req.body.address
-
-  // Read wallet address (publicAddress)
+  // read out wallet address
+  const wallet = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../wallet/sealer.json'), 'utf8'))
+  const addressToRegister = wallet.address
 
   // Send wallet address to authority backend to register
+  try {
+    await axios.post(config.authBackend.devUrl + '/chainspec', {
+      address: addressToRegister,
+    })
+    res.status(200).json({ msg: WALLET_REGISTERED_MSG, address: addressToRegister })
+  } catch (error) {
+    res.status(400).json({ msg: WALLET_NOT_REGISTERED_MSG })
+  }
 
-  // subscribe to changes
-
-  // as soon as all nodes have registered themselves with the authority backend,
-  // this backend should fetch the final chainspec.json
-
-  res.status(200).json({ msg: WALLET_REGISTERED_MSG })
+  // TODO: if possible subscribe to nr of registered sealer -> to know when to download chainspec
 })
 
 export default router
