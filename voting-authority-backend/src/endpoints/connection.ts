5364e4bdd207ee002d367ac6caab0bd61315f265
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2019-12-10 11:49:50 +0100

    vote-auth-be - change /connectionNode to POST and record urls

--- voting-authority-backend/src/endpoints/connection.ts
+++ voting-authority-backend/src/endpoints/connection.ts
@@ -1,27 +1,43 @@
 import express from 'express'
-import { getValueFromDB, STATE_TABLE } from '../database/database'
-import { parityConfig } from '../config'
+import { addToList, getValueFromDB, STATE_TABLE, NODES_TABLE } from '../database/database'
 import { VotingState } from './state'
 
 const TOO_EARLY_MSG: string = 'You are too early to connect. Please wait for the STARTUP stage.'
 const ON_TIME_MSG: string = "Let's connect and spin up that network!."
 const TOO_LATE_MSG: string = 'You are too late to connect. You should have done that during the STARTUP stage.'
 
+const URL_WRONG_FORMAT: string = 'The provided URL has the wrong format!'
+const URL_ALREADY_STORED: string = 'The provided URL is already stored!'
+
 const router: express.Router = express.Router()
 
-router.get('/connectionNode', async (req, res) => {
-  const nodeUrl: string = parityConfig.connectionNodeUrl
+router.post('/connectionNode', async (req, res) => {
+  const url: string = req.body.url
   const currentState: string = <string>getValueFromDB(STATE_TABLE)
+  const nodes: string[] = getValueFromDB(NODES_TABLE)
 
   let msg: string = ''
   switch (currentState) {
     case VotingState.REGISTER:
-      msg = TOO_EARLY_MSG
-      break
+      res.status(400).json({ msg: TOO_EARLY_MSG })
+      return
     case VotingState.STARTUP:
-      msg = ON_TIME_MSG
-      break
+      // check the url format
+      // http://abcdef:1234
+      if (!url.match(/^http:\/\/.*:\d{4}$/)) {
+        res.status(400).json({ msg: URL_WRONG_FORMAT, yourUrl: url })
+        return
+      }
 
+      // only add the url if it is not already stored
+      if (nodes.includes(url)) {
+        res.status(400).json({ msg: URL_ALREADY_STORED, yourUrl: url, connectTo: nodes[0] })
+        return
+      } else {
+        addToList(NODES_TABLE, [url])
+        res.status(200).json({ msg: ON_TIME_MSG, yourUrl: url, connectTo: nodes[0] })
+        return
+      }
     case VotingState.CONFIG:
       msg = TOO_LATE_MSG
       break
@@ -32,7 +48,7 @@ router.get('/connectionNode', async (req, res) => {
       msg = TOO_LATE_MSG
       break
   }
-  res.status(200).json({ msg: msg, nodeUrl: nodeUrl })
+  res.status(200).json({ msg: msg, yourUrl: url, connectTo: nodes[0] })
 })
 
 export default router
