e02d78b824fea959427105b8981050ceacc31c5d
Moritz Eck <moritz.eck@gmail.com>
2019-12-12 00:54:49 +0100

    Sealer
    - update: docker network creation
    - update: docker-compose.yml
    - create nodes with specific IPs for specific networks
    - TODO: update the IPs in the backend and frontend accordingly

--- sealer/stop-sealer.sh
+++ sealer/stop-sealer.sh
@@ -3,10 +3,29 @@ readonly dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 # input
 sealerNr=$1
+internal=10
 
-# create env file for docker-compose
-echo BACKEND_PORT=401$sealerNr >> $dir/.env
+# ports
+echo SEALER_BE_PORT=401$sealerNr >> $dir/.env
 echo FRONTEND_PORT=301$sealerNr >> $dir/.env
+
+# internal IP's for BE <-> FE
+echo SEALER_BE_IP=172.1.$internal$sealerNr.20 >> $dir/.env
+echo FRONTEND_IP=172.1.$internal$sealerNr.10 >> $dir/.env
+echo INTERNAL_IP_RANGE=172.1.$internal$sealerNr.0/24 >> $dir/.env
+
+# ip and port of Vote Authority Backend (vote-auth network)
+echo VOTH_AUTH_BE_IP=172.1.10.5 >> $dir/.env
+echo VOTH_AUTH_BE_PORT=4001 >> $dir/.env
+
+# ip of Sealer Backend in vot-auth network
+echo VOTE_AUTH_NETWORK_IP=172.1.10.$sealerNr$internal >> $dir/.env
+
+# ip of Sealer's Parity Node (parity-node network)
+echo PARITY_NODE_IP=172.1.1.$sealerNr >> $dir/.env
+
+# ip of Sealer Backend in parity-node network
+echo PARITY_NETWORK_IP=172.1.1.10$sealerNr >> $dir/.env
 
 # shutdown everything the docker-compose up started, includes network created + services
 docker-compose -p controller_$sealerNr -f docker-compose.yml down
