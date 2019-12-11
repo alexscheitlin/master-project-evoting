903aaef272227a9a6e9ddf41fc5870278df9d41e
Moritz Eck <moritz.eck@gmail.com>
2019-12-12 00:50:26 +0100

    Voting-Authority
    - create docker network with fixed IP range
    - assign IP according to .env
    - adjust .env variables in frontend and backend

--- voting-authority/start-auth.sh
+++ voting-authority/start-auth.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 readonly name=$(basename $0)
 readonly dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+readonly parentDir="$(dirname "$dir")"
 readonly network_name="vote-auth"
 
 # Voting Authority
@@ -8,30 +9,54 @@ readonly network_name="vote-auth"
 # Backend     port 4001
 # Frontend    port 3001
 
+# get crypto library into the mix
+# build it and copy the dist folder into the backend
+cryptoPath=$parentDir/crypto
+cp -r $cryptoPath $dir/backend/mp-crypto
+
+rm -rf $dir/backend/mp-crypto/node_modules
+rm -rf $dir/backend/mp-crypto/dist
+
 # create env file for backend where to find ports
-echo NODE_ENV=development >> $dir/backend/envs/.env.development
+mkdir $dir/backend/envs
 echo BACKEND_PORT=4001 >> $dir/backend/envs/.env.development
 echo FRONTEND_PORT=3001 >> $dir/backend/envs/.env.development
 
 # create a production env file
 cp $dir/backend/envs/.env.development $dir/backend/envs/.env.production
 
+# add the NODE_ENV mode to the respective files
+echo NODE_ENV=development >> $dir/backend/envs/.env.development
+echo NODE_ENV=production >> $dir/backend/envs/.env.production
+
 # create env file for docker-compose to find ports
 echo BACKEND_PORT=4001 >> $dir/.env
 echo FRONTEND_PORT=3001 >> $dir/.env
+
+# internal IP's for BE <-> FE
+echo BACKEND_IP=172.1.11.20 >> $dir/.env
+echo FRONTEND_IP=172.1.11.10 >> $dir/.env
+
+# external IP for VoteAuthBE <-> Sealer BEs
+echo VOTE_AUTH_BACKEND_IP=172.1.10.5 >> $dir/.env
 
 # check if parity-nodes docker network exists, otherwise create it
 if [[ $(docker network ls | xargs | grep -q $network_name) == 0 ]]; then
     echo "network: $network_name exists!"
 else
     echo "creating network: $network_name"
-    docker network create $network_name > /dev/null 2>&1
+    docker network create $network_name \
+    --driver=bridge \
+    --subnet=172.1.10.0/24 \
+    --gateway=172.1.10.1 \
+    --opt com.docker.network.bridge.enable_icc=true \
+    --opt com.docker.network.bridge.enable_ip_masquerade=true > /dev/null 2>&1
 fi
 
 # start docker containers
 docker-compose -p vote-auth -f docker-compose.yml up --build --detach
 
 # remove all temp files
-rm -f $dir/backend/envs/.env.development
-rm -f $dir/backend/envs/.env.production
+rm -rf $dir/backend/envs
+rm -rf $dir/backend/mp-crypto
 rm -f $dir/.env
