eca4ffc46138767ffdd3eb1d89bcc31ac4f0af41
Moritz Eck <moritz.eck@gmail.com>
2019-12-12 00:52:39 +0100

    Parity-Node
    - update network creation -> distinct ip range
    - assign specific IPs to nodes when they are created (start with 1 instead of 0)

--- poa-blockchain/scripts/register-node.sh
+++ poa-blockchain/scripts/register-node.sh
@@ -5,14 +5,14 @@
 ###############################################################################
 
 # obtain enode of node at specified port
-host="localhost:$1"
+host="172.1.0.$1"
 method="parity_enode"
 params=""
 enode=$(./execute-rpc-call.sh "$host" "$method" "$params" | jq .result)
 echo $enode
 
 # register node
-host="localhost:$2"
+host="172.1.0.$2"
 method="parity_addReservedPeer"
 params="$enode"
 result=$(./execute-rpc-call.sh "$host" "$method" "$params")
