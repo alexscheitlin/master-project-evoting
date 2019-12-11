b5aa5fc209e80b5009d8820bdcfe7568a2ac0ec3
Moritz Eck <moritz.eck@gmail.com>
2019-12-12 00:50:26 +0100

    Voting-Authority
    - create docker network with fixed IP range
    - assign IP according to .env
    - adjust .env variables in frontend and backend

--- voting-authority/frontend/src/constants.ts
+++ voting-authority/frontend/src/constants.ts
@@ -1,1 +1,2 @@
-export const DEV_URL: string = 'http://localhost:4001';
+// TODO: check if this is correct
+export const DEV_URL: string = `http://${process.env.REACT_APP_BACKEND_IP}:${process.env.REACT_APP_BACKEND_PORT}`;
--- voting-authority/stop-auth.sh
+++ voting-authority/stop-auth.sh
@@ -6,6 +6,13 @@ readonly dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 echo BACKEND_PORT=4001 >> $dir/.env
 echo FRONTEND_PORT=3001 >> $dir/.env
 
+# internal IP's for BE <-> FE
+echo BACKEND_IP=172.1.11.20 >> $dir/.env
+echo FRONTEND_IP=172.1.11.10 >> $dir/.env
+
+# external IP for VoteAuthBE <-> Sealer BEs
+echo VOTE_AUTH_BACKEND_IP=172.1.10.5 >> $dir/.env
+
 # shutdown everything the docker-compose up started, includes network created + services
 docker-compose -p vote-auth -f docker-compose.yml down
 
