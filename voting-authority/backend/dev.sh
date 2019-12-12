5aa5b08aec3d3d7056373dacb66e6ec0d4c02fa3
nikzaugg <zauggnik@gmail.com>
2019-12-12 21:16:22 +0100

    auth backend - add scripts for local development

---
+++ voting-authority/backend/dev.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+########################################
+# relative directories
+########################################
+readonly name=$(basename $0)
+readonly dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+readonly parentDir="$(dirname "$dir")"
+readonly parentParentDir="$(dirname "$parentDir")"
+
+
+###########################################
+# Cleanup
+###########################################
+rm -f $dir/.env
+
+###########################################
+# Mode
+###########################################
+mode=development
+echo "The mode is: $mode"
+
+###########################################
+# Config
+# - the config file with all IPs and ports
+###########################################
+globalConfig=$parentParentDir/system.json
+
+###########################################
+# ENV variables
+###########################################
+# - Voting Authority Backend PORT (the port stays the same, in dev and prod mode) 
+VOTING_AUTH_BACKEND_PORT=$(cat $globalConfig | jq .services.voting_authority_backend.port)
+# - Voting Authority Backend IP (either 172.1.1.XXX or localhost)
+VOTING_AUTH_BACKEND_IP=$(cat $globalConfig | jq .services.voting_authority_backend.ip.$mode | tr -d \")
+# - Voting Authority Frontend PORT (the port stays the same, in dev and prod mode)
+VOTING_AUTH_FRONTEND_PORT=$(cat $globalConfig | jq .services.voting_authority_frontend.port)
+# - Voting Authority Frontend IP (either 172.1.1.XXX or localhost)
+VOTING_AUTH_FRONTEND_IP=$(cat $globalConfig | jq .services.voting_authority_frontend.ip.$mode | tr -d \")
+# - POA Blockchain Main RPC PORT (the port stays the same, in dev and prod mode)
+PARITY_NODE_PORT=$(cat $globalConfig | jq .services.sealer_1_parity.port)
+# - POA Blockchain Main RPC IP (either 172.1.1.XXX or localhost)
+PARITY_NODE_IP=$(cat $globalConfig | jq .services.sealer_1_parity.ip.$mode | tr -d \")
+# - Specify NODE_ENV
+NODE_ENV=$mode
+
+###########################################
+# write ENV variables into .env
+###########################################
+echo VOTING_AUTH_BACKEND_PORT=${VOTING_AUTH_BACKEND_PORT} >> $dir/.env
+echo VOTING_AUTH_BACKEND_IP=${VOTING_AUTH_BACKEND_IP} >> $dir/.env
+echo VOTING_AUTH_FRONTEND_PORT=${VOTING_AUTH_FRONTEND_PORT} >> $dir/.env
+echo VOTING_AUTH_FRONTEND_IP=${VOTING_AUTH_FRONTEND_IP} >> $dir/.env
+echo PARITY_NODE_PORT=${PARITY_NODE_PORT} >> $dir/.env
+echo PARITY_NODE_IP=${PARITY_NODE_IP} >> $dir/.env
+echo NODE_ENV=${NODE_ENV} >> $dir/.env
+
+
+###########################################
+# making sure mp-crypto is linked
+###########################################
+echo "##########################################################################"
+echo "# Linking mp-crypto, running the commands below                          "
+echo "##########################################################################"
+echo "# > cd $parentParentDir/crypto                                             "
+echo "# > npm link                                                               "
+echo "# > cd $dir                                                                "
+echo "# > npm link mp-crypto                                                     "
+echo "##########################################################################"
+cd $parentParentDir/crypto
+npm link
+cd $dir
+npm link mp-crypto
+
+###########################################
+# start dev server
+###########################################
+cd $dir
+npm run serve:dev:clean
