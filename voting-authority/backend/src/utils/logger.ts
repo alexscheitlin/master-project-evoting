4e30fa46bc580919b7e781ff995aa86597b3a4c0
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2019-12-12 14:54:39 +0100

    vote-auth-be - fix auto merging

--- voting-authority/backend/src/utils/logger.ts
+++ voting-authority/backend/src/utils/logger.ts
@@ -1,12 +1,39 @@
 import express from 'express'
 
-const logger = (request: express.Request, response: express.Response, next: express.NextFunction) => {
+export const requestLogger = (request: express.Request, response: express.Response, next: express.NextFunction): void => {
+  console.log('---')
   if (request.method === 'POST') {
-    console.log(`${request.method} ${request.path} ${JSON.stringify(request.body)}`)
+    console.log('req', request.method, request.path, '   ', JSON.stringify(request.body))
   } else {
-    console.log(`${request.method} ${request.path}`)
+    console.log('req', request.method, request.path)
   }
   next()
 }
 
-export default logger
+export const responseLogger = (request: express.Request, response: express.Response, next: express.NextFunction): void => {
+  let oldWrite = response.write
+  let oldEnd = response.end
+
+  let chunks: any[] = []
+
+  response.write = function(chunk: any): boolean {
+    chunks.push(chunk)
+
+    // @ts-ignore
+    return oldWrite.apply(response, arguments)
+  }
+
+  response.end = function(chunk: any): boolean {
+    if (chunk) {
+      chunks.push(chunk)
+    }
+
+    const body = Buffer.concat(chunks).toString('utf8')
+    console.log('res', request.method, request.path, response.statusCode, body)
+
+    // @ts-ignore
+    return oldEnd.apply(response, arguments)
+  }
+
+  next()
+}
