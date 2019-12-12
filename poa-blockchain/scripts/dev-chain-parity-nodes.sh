090af89a039ce940e0654f1dd06b8df013ed79f4
nikzaugg <zauggnik@gmail.com>
2019-12-13 00:26:48 +0100

    sealer parity - update run.sh and docker configuration

--- poa-blockchain/scripts/dev-chain-parity-nodes.sh
+++ poa-blockchain/scripts/dev-chain-parity-nodes.sh
@@ -5,6 +5,18 @@ readonly dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 readonly parentDir="$(dirname "$dir")"
 readonly parentParentDir="$(dirname "$parentDir")"
 
+###########################################
+# Config
+# - the config file with all IPs and ports
+###########################################
+globalConfig=$parentParentDir/system.json
+
+###########################################
+# Mode
+###########################################
+mode=production
+echo "The mode is: $mode"
+
 # We need to provide a chain.json to the parity nodes
 # in order for them to boot up. And as we are not manually want to fetch the chain.json from
 # the authority backend, we will copy the example.chain.json inside sealer/backend/src/chainspec
@@ -34,6 +46,9 @@ sleep 1
 
 cd $parentParentDir/poa-blockchain/scripts
 
+###########################################
+# ENODE registration
+###########################################
 printf  "Register nodes "
 # wait until containers are ready to answer rpc calls
 sleep 1; printf "."
@@ -45,7 +60,22 @@ sleep 1; printf "."
 sleep 1; printf "."
 echo
 
-$dir/register-node.sh 12:7012 11:7011
-$dir/register-node.sh 13:7013 11:7011
+parity1_IP=$(cat $globalConfig | jq .services.sealer_parity_1.ip.$mode | tr -d \")
+parity1_PORT=$(cat $globalConfig | jq .services.sealer_parity_1.port)
 
-echo "Done!"
+parity2_IP=$(cat $globalConfig | jq .services.sealer_parity_2.ip.$mode | tr -d \")
+parity2_PORT=$(cat $globalConfig | jq .services.sealer_parity_2.port)
+
+parity3_IP=$(cat $globalConfig | jq .services.sealer_parity_3.ip.$mode | tr -d \")
+parity3_PORT=$(cat $globalConfig | jq .services.sealer_parity_3.port)
+
+bootnode_address="http://$parity1_IP:$parity1_PORT"
+
+echo "sending rpc call to " $bootnode_address
+echo "http://$parity2_IP:$parity2_PORT"
+echo "http://$parity3_IP:$parity3_PORT"
+
+$dir/register-node.sh "http://$parity2_IP:$parity2_PORT" $bootnode_address
+$dir/register-node.sh "http://$parity3_IP:$parity3_PORT" $bootnode_address
+
+echo "Chain Setup Done. You should have three sealers running."
--- poa-blockchain/scripts/register-node.sh
+++ poa-blockchain/scripts/register-node.sh
@@ -5,14 +5,14 @@
 ###############################################################################
 
 # obtain enode of node at specified port
-host="http://172.1.1.$1"
+host="$1"
 method="parity_enode"
 params=""
 enode=$(./execute-rpc-call.sh "$host" "$method" "$params" | jq .result)
 echo $enode
 
 # register node
-host="http://172.1.1.$2"
+host="$2"
 method="parity_addReservedPeer"
 params="$enode"
 result=$(./execute-rpc-call.sh "$host" "$method" "$params")
