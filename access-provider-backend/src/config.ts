7158d83b166f891e8fbee9e9d8b077cf0fccbf79
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2019-12-13 21:45:06 +0100

    access-provider-be - add voting auth be ip/port to docker/dev config to get ballot address from voting auth be

--- access-provider-backend/src/config.ts
+++ access-provider-backend/src/config.ts
@@ -4,6 +4,7 @@ import { config } from 'dotenv'
 config()
 
 export const serverConfig = {
+  authUrl: `http://${process.env.VOTING_AUTH_BACKEND_IP}:${process.env.VOTING_AUTH_BACKEND_PORT}`,
   nodeUrl: `http://${process.env.PARITY_NODE_IP}:${process.env.PARITY_NODE_PORT}`,
   accountAddress: '0x004661de90cd1dcb998e8464a0f3c3da9f085950',
   accountPassword: 'access',
