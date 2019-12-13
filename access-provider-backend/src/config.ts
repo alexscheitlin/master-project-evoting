eabbc68015accc364f666b9ac80c4033ad4595e5
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2019-12-13 21:18:18 +0100

    access-provider-be - create prefunded wallet and add config file

---
+++ access-provider-backend/src/config.ts
@@ -0,0 +1,9 @@
+import { config } from 'dotenv'
+
+// Important. This is needed, else the provess.env variables are undefined
+config()
+
+export const serverConfig = {
+  accountAddress: '0x004661de90cd1dcb998e8464a0f3c3da9f085950',
+  accountPassword: 'access',
+}
