ff737ff8cc4b4507c48e6bebeb6113b59c1ba807
Moritz Eck <moritz.eck@gmail.com>
2019-12-14 17:57:49 +0100

    Sealer Frontend
    - add: tally votes screen
    - add: request to sealer backend /decrypt

--- sealer/frontend/src/components/TallyVotes/TallyVotes.tsx
+++ sealer/frontend/src/components/TallyVotes/TallyVotes.tsx
@@ -1,7 +1,73 @@
-import React from "react";
+import { Button, createStyles, makeStyles, Theme, Typography } from '@material-ui/core';
+import React, { useState } from 'react';
+import { LoadSuccess } from '../shared/LoadSuccess';
+import { StepTitle } from '../shared/StepTitle';
+
+import { ErrorSnackbar } from '../Helpers/ErrorSnackbar';
+import { SealerBackend } from '../../services';
 
 interface Props {}
 
 export const TallyVotes: React.FC<Props> = () => {
-  return <div>TallyVotes</div>;
+  const classes = useStyles();
+
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  const [hasError, setHasError] = useState<boolean>(false);
+
+  const [loading, setLoading] = useState<boolean>(false);
+  const [success, setSuccess] = useState<boolean>(false);
+
+  const submitDecryptedShare = async () => {
+    try {
+      setLoading(true);
+      const response = await SealerBackend.decryptShare();
+      setLoading(false);
+      setSuccess(true);
+
+      console.log(response);
+    } catch (error) {
+      setHasError(true);
+      setErrorMessage(error.msg);
+    }
+  };
+
+  return (
+    <div className={classes.root}>
+      <StepTitle title="Tally Votes" />
+      <div className={classes.wrapper}>
+        <Button className={classes.button} variant="contained" onClick={submitDecryptedShare}>
+          Submit Decrypted Share
+        </Button>
+      </div>
+      <div className={classes.loader}>
+        <LoadSuccess loading={loading} success={success} />
+      </div>
+
+      {hasError && <ErrorSnackbar open={hasError} message={errorMessage} />}
+    </div>
+  );
 };
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      position: 'relative',
+    },
+    wrapper: {
+      display: 'flex',
+      alignItems: 'center',
+    },
+    button: {
+      marginRight: theme.spacing(1),
+    },
+    statusButtonWrapper: {},
+    sealerInfo: {
+      padding: theme.spacing(3, 0),
+    },
+    loader: {
+      position: 'absolute',
+      bottom: 0,
+      right: 0,
+    },
+  })
+);
