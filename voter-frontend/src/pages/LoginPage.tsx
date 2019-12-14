fc19365f826f2f17b8f1be8bf1e297ab77a92da8
nikzaugg <zauggnik@gmail.com>
2019-12-14 21:52:21 +0100

    voter frontend - refactoring and simplify page change logic

--- voter-frontend/src/pages/LoginPage.tsx
+++ voter-frontend/src/pages/LoginPage.tsx
@@ -3,14 +3,8 @@ import React, { useState } from 'react';
 import LoginForm from '../components/LoginForm/LoginForm';
 import { EIdentityProviderService } from '../services';
 import { useVoterStore } from '../store';
-import LoadingPage from './LoadingPage';
 
-interface Props {
-  onLoadFinished: () => void;
-}
-
-const LoginPage: React.FC<Props> = ({ onLoadFinished }) => {
-  const [loginSubmitted, setLoginSubmitted] = useState(false);
+const LoginPage: React.FC = () => {
   const voterState = useVoterStore();
 
   const handleLogin = async (username: string, password: string) => {
@@ -18,15 +12,12 @@ const LoginPage: React.FC<Props> = ({ onLoadFinished }) => {
       const token = await EIdentityProviderService.getToken(username, password);
       voterState.setToken(token);
       voterState.setAuthenicated(true);
-      setLoginSubmitted(true);
     } catch (error) {
       console.log(error);
     }
   };
 
-  return (
-    <div>{loginSubmitted ? <LoadingPage onSetupComplete={onLoadFinished} /> : <LoginForm onLogin={handleLogin} />}</div>
-  );
+  return <LoginForm onLogin={handleLogin} />;
 };
 
 export default LoginPage;
--- voter-frontend/src/store/store.ts
+++ voter-frontend/src/store/store.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
 
-export const [useVoterStore] = create(set => ({
+export const [useVoterStore] = create((set, get) => ({
   authenticated: false,
   token: '',
   wallet: '',
@@ -8,10 +8,118 @@ export const [useVoterStore] = create(set => ({
   contractAddress: '',
   connectionNodeUrl: '',
 
-  setAuthenicated: (auth: boolean) => set({ authenticated: auth }),
-  setToken: (token: string) => set({ token: token }),
-  setWallet: (wallet: string) => set({ wallet: wallet }),
-  setContract: (contract: any) => set({ contract: contract }),
-  setBallotContractAddress: (address: string) => set({ contractAddress: address }),
-  setConnectionNodeUrl: (url: string) => set({ connectionNodeUrl: url }),
+  // -----------------------------------
+  // LOGOUT
+  // -----------------------------------
+  logout: () => {
+    localStorage.clear();
+    set({ authenticated: false });
+  },
+
+  // -----------------------------------
+  // AUTHENTICATION
+  // -----------------------------------
+  isAuthenticated: () => {
+    const isAuth = localStorage.getItem('authenticated');
+    if (isAuth === 'true') {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  setAuthenicated: (auth: boolean) => {
+    set({ authenticated: auth });
+    localStorage.setItem('authenticated', auth.toString());
+  },
+
+  // -----------------------------------
+  // TOKEN
+  // -----------------------------------
+  isTokenSet: () => {
+    const token = localStorage.getItem('token');
+    if (token === null || token === '') {
+      return false;
+    } else {
+      return true;
+    }
+  },
+  getToken: () => {
+    if (get().isTokenSet()) {
+      return localStorage.getItem('token');
+    }
+  },
+  setToken: (token: string) => {
+    set({ token: token });
+    localStorage.setItem('token', token);
+  },
+
+  // -----------------------------------
+  // WALLET
+  // -----------------------------------
+  isWalletSet: () => {
+    const wallet = localStorage.getItem('wallet');
+    if (wallet === null || wallet === '') {
+      return false;
+    } else {
+      return true;
+    }
+  },
+  getWallet: () => {
+    if (get().isWalletSet()) {
+      return localStorage.getItem('wallet');
+    }
+  },
+  setWallet: (wallet: string) => {
+    set({ wallet: wallet });
+    localStorage.setItem('wallet', wallet);
+  },
+
+  // -----------------------------------
+  // CONTRACT
+  // -----------------------------------
+  setContract: (contract: any) => {
+    set({ contract: contract });
+  },
+
+  // -----------------------------------
+  // BALLOT CONTRACT ADDRESS
+  // -----------------------------------
+  isBallotContractAddressSet: () => {
+    const address = localStorage.getItem('contractAddress');
+    if (address === null || address === '') {
+      return false;
+    } else {
+      return true;
+    }
+  },
+  getBallotContractAddress: () => {
+    if (get().isBallotContractAddressSet()) {
+      return localStorage.getItem('contractAddress');
+    }
+  },
+  setBallotContractAddress: (address: string) => {
+    set({ contractAddress: address });
+    localStorage.setItem('contractAddress', address);
+  },
+
+  // -----------------------------------
+  // CONNECTION NODE
+  // -----------------------------------
+  isConnectionNodeUrlSet: () => {
+    const url = localStorage.getItem('connectionNodeUrl');
+    if (url === null || url === '') {
+      return false;
+    } else {
+      return true;
+    }
+  },
+  getConnectionNodeUrl: () => {
+    if (get().isConnectionNodeUrlSet()) {
+      return localStorage.getItem('connectionNodeUrl');
+    }
+  },
+  setConnectionNodeUrl: (url: string) => {
+    set({ connectionNodeUrl: url });
+    localStorage.setItem('connectionNodeUrl', url);
+  },
 }));
