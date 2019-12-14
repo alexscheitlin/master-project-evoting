1ad9525e4e19f613e8000069b5d6ed9ec98ec94e
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2019-12-14 23:26:33 +0100

    access-provider-be - remove production mode and clean up

--- access-provider-backend/src/server.ts
+++ access-provider-backend/src/server.ts
@@ -1,9 +1,6 @@
 import cors from 'cors'
 import { config } from 'dotenv'
 import express from 'express'
-import fs from 'fs'
-import helmet from 'helmet'
-import https from 'https'
 
 import { setupDB } from './database/database'
 import nodeUrl from './endpoints/nodeUrl'
@@ -29,38 +26,7 @@ server.use('/', sendTokens)
 // setup the database
 setupDB()
 
-const isProduction = false
-if (isProduction) {
-  // adds the "Strict-Transport-Security" header.
-  server.use(
-    helmet.hsts({
-      // one year
-      maxAge: 31536000,
-      includeSubDomains: true,
-      force: true,
-    })
-  )
-
-  // certificates need to be loaded as well
-  const options: https.ServerOptions = {
-    key: fs.readFileSync('./keys/cert/localhost.key'),
-    cert: fs.readFileSync('./keys/cert/localhost.crt'),
-    passphrase: process.env.passphrase,
-    minVersion: 'TLSv1.2',
-    ciphers: ['ECDHE-ECDSA-AES256-GCM-SHA384', 'ECDHE-ECDSA-AES128-GCM-SHA256', '!RC4', '!MD5', '!aNULL'].join(':'),
-    ecdhCurve: 'prime256v1',
-
-    // must be enabled if client certificate shall be requested
-    requestCert: false,
-  }
-
-  // we will pass our 'server' to 'https'
-  https.createServer(options, server).listen(process.env.ACCESS_PROVIDER_BACKEND_PORT, () => {
-    console.log(`HTTPS server started at https://${process.env.ACCESS_PROVIDER_BACKEND_IP}:${process.env.ACCESS_PROVIDER_BACKEND_PORT}`)
-  })
-} else {
-  // start the Express server
-  server.listen(process.env.ACCESS_PROVIDER_BACKEND_PORT, () => {
-    console.log(`HTTP server started at http://${process.env.ACCESS_PROVIDER_BACKEND_IP}:${process.env.ACCESS_PROVIDER_BACKEND_PORT}`)
-  })
-}
+// start the Express server
+server.listen(process.env.ACCESS_PROVIDER_BACKEND_PORT, () => {
+  console.log(`HTTP server started at http://${process.env.ACCESS_PROVIDER_BACKEND_IP}:${process.env.ACCESS_PROVIDER_BACKEND_PORT}`)
+})
