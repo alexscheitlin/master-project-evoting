8a673826003b1a30406352bec37e12d96fd28c23
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2019-12-14 23:37:53 +0100

    identity-provider-be - remove production mode and clean up

--- identity-provider-backend/README.md
+++ identity-provider-backend/README.md
@@ -27,23 +27,3 @@ Run `npm run test` to run all test of the project or `npm run test:watch` to con
 ### Endpoint
 
 TODO
-
-<!--
-### Server Certificate
-
-For the server to run in a realisitc setup (i.e. using HTTPS and TLS) a certificate is required.
-Generate the certificate using:
-
-```bash
-# 1. Select what elliptic curve to use
-openssl ecparam -list_curves
-
-# 2. Generate the necessary public/private key pair (elliptic-curve)
-openssl ecparam -name {name_of_the_curve} -genkey -out key.pem
-
-# 3. Create a new certificate
-openssl req -x509 -new -key key.pem -out cert.pem -days 365 -nodes -SHA384
-```
-
-Place the certificate and key in the folder: `./keys/cert` and add the passphrase to the `.env.production` file.
--->
--- identity-provider-backend/src/server.ts
+++ identity-provider-backend/src/server.ts
@@ -1,14 +1,11 @@
-import cors from 'cors';
-import { config } from 'dotenv';
-import express from 'express';
-import fs from 'fs';
-import helmet from 'helmet';
-import https from 'https';
+import cors from 'cors'
+import { config } from 'dotenv'
+import express from 'express'
 
-import { setupDB } from './database/database';
-import getToken from './endpoints/getToken';
-import register from './endpoints/register';
-import { requestLogger, responseLogger } from './utils/logger';
+import { setupDB } from './database/database'
+import getToken from './endpoints/getToken'
+import register from './endpoints/register'
+import { requestLogger, responseLogger } from './utils/logger'
 
 config()
 
@@ -27,38 +24,7 @@ server.use('/', getToken)
 // setup the database
 setupDB()
 
-const isProduction = false
-if (isProduction) {
-  // adds the "Strict-Transport-Security" header.
-  server.use(
-    helmet.hsts({
-      // one year
-      maxAge: 31536000,
-      includeSubDomains: true,
-      force: true,
-    })
-  )
-
-  // certificates need to be loaded as well
-  const options: https.ServerOptions = {
-    key: fs.readFileSync('./keys/cert/localhost.key'),
-    cert: fs.readFileSync('./keys/cert/localhost.crt'),
-    passphrase: process.env.passphrase,
-    minVersion: 'TLSv1.2',
-    ciphers: ['ECDHE-ECDSA-AES256-GCM-SHA384', 'ECDHE-ECDSA-AES128-GCM-SHA256', '!RC4', '!MD5', '!aNULL'].join(':'),
-    ecdhCurve: 'prime256v1',
-
-    // must be enabled if client certificate shall be requested
-    requestCert: false,
-  }
-
-  // we will pass our 'server' to 'https'
-  https.createServer(options, server).listen(process.env.IDENTITY_PROVIDER_BACKEND_PORT, () => {
-    console.log(`HTTPS server started at https://${process.env.IDENTITY_PROVIDER_BACKEND_IP}:${process.env.IDENTITY_PROVIDER_BACKEND_PORT}`)
-  })
-} else {
-  // start the Express server
-  server.listen(process.env.IDENTITY_PROVIDER_BACKEND_PORT, () => {
-    console.log(`HTTP server started at http://${process.env.IDENTITY_PROVIDER_BACKEND_IP}:${process.env.IDENTITY_PROVIDER_BACKEND_PORT}`)
-  })
-}
+// start the Express server
+server.listen(process.env.IDENTITY_PROVIDER_BACKEND_PORT, () => {
+  console.log(`HTTP server started at http://${process.env.IDENTITY_PROVIDER_BACKEND_IP}:${process.env.IDENTITY_PROVIDER_BACKEND_PORT}`)
+})
