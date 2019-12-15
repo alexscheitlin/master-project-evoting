9960156e0a3fc88fa395ee5275feaa15c35a6ccd
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2019-12-15 09:08:45 +0100

    vote-auth-be - remove production mode and clean up

--- voting-authority/backend/src/server.ts
+++ voting-authority/backend/src/server.ts
@@ -1,9 +1,6 @@
 import cors from 'cors'
 import { config } from 'dotenv'
 import express from 'express'
-import fs from 'fs'
-import helmet from 'helmet'
-import https from 'https'
 
 import { setupDB } from './database/database'
 import chainspec from './endpoints/chainspec'
@@ -40,10 +37,11 @@ server.use(
       `http://172.1.1.172:7012`,
       `http://localhost:7012`,
       `http://172.1.1.171:7011`,
-      `http://localhost:7011`
+      `http://localhost:7011`,
     ],
   })
 )
+
 // add all routes
 server.use('/', chainspec)
 server.use('/', connection)
@@ -54,39 +52,8 @@ server.use('/', state)
 // setup the database
 setupDB()
 
-const isProduction = false
-if (isProduction) {
-  // adds the "Strict-Transport-Security" header.
-  server.use(
-    helmet.hsts({
-      // one year
-      maxAge: 31536000,
-      includeSubDomains: true,
-      force: true,
-    })
-  )
-
-  // certificates need to be loaded as well
-  const options: https.ServerOptions = {
-    key: fs.readFileSync('./keys/cert/localhost.key'),
-    cert: fs.readFileSync('./keys/cert/localhost.crt'),
-    passphrase: process.env.passphrase,
-    minVersion: 'TLSv1.2',
-    ciphers: ['ECDHE-ECDSA-AES256-GCM-SHA384', 'ECDHE-ECDSA-AES128-GCM-SHA256', '!RC4', '!MD5', '!aNULL'].join(':'),
-    ecdhCurve: 'prime256v1',
-
-    // must be enabled if client certificate shall be requested
-    requestCert: false,
-  }
-
-  // we will pass our 'server' to 'https'
-  https.createServer(options, server).listen(process.env.VOTING_AUTH_BACKEND_PORT, () => {
-    console.log(`HTTPS server started at https://${process.env.VOTING_AUTH_BACKEND_IP}:${process.env.VOTING_AUTH_BACKEND_PORT}`)
-  })
-} else {
-  // start the Express server
-  server.listen({ port: process.env.VOTING_AUTH_BACKEND_PORT }, () => {
-    console.log(`HTTP server started at http://${process.env.VOTING_AUTH_BACKEND_IP}:${process.env.VOTING_AUTH_BACKEND_PORT}`)
-    console.log('FRONTEND RUNS ON:', `http://${process.env.VOTING_AUTH_FRONTEND_IP}:${process.env.VOTING_AUTH_FRONTEND_PORT}`)
-  })
-}
+// start the Express server
+server.listen({ port: process.env.VOTING_AUTH_BACKEND_PORT }, () => {
+  console.log(`HTTP server started at http://${process.env.VOTING_AUTH_BACKEND_IP}:${process.env.VOTING_AUTH_BACKEND_PORT}`)
+  console.log('FRONTEND RUNS ON:', `http://${process.env.VOTING_AUTH_FRONTEND_IP}:${process.env.VOTING_AUTH_FRONTEND_PORT}`)
+})
