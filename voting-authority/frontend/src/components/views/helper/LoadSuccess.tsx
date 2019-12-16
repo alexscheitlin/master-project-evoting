95ad67bb5054de79e644c7f9217aee27268f47ac
nikzaugg <zauggnik@gmail.com>
2019-12-16 14:17:17 +0100

    auth fe - add optional white color to loader

--- voting-authority/frontend/src/components/views/helper/LoadSuccess.tsx
+++ voting-authority/frontend/src/components/views/helper/LoadSuccess.tsx
@@ -6,12 +6,13 @@ import React from 'react';
 interface Props {
   loading?: boolean;
   success?: boolean;
+  white?: boolean;
 }
 
-export const LoadSuccess: React.FC<Props> = ({ loading, success }) => {
+export const LoadSuccess: React.FC<Props> = ({ loading, success, white }) => {
   return (
     <div>
-      {loading && <CircularProgress size={24} />}
+      {loading && <CircularProgress size={24} style={{ color: white ? 'white' : '' }} />}
       {success && <CheckCircleIcon style={{ fontSize: 24, color: green[500] }} />}
     </div>
   );
