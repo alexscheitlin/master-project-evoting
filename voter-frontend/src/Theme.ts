8f7b2de865bd06aad03553a36326963c56fdcf1c
nikzaugg <zauggnik@gmail.com>
2019-12-17 10:31:09 +0100

    voter frontend - add theme and new favicon

---
+++ voter-frontend/src/Theme.ts
@@ -0,0 +1,25 @@
+import red from '@material-ui/core/colors/red';
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
+
+// A custom theme for this app
+const mainTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#8F799C',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: '#fff',
+    },
+  },
+  typography: {
+    fontSize: 16,
+  },
+});
+
+export default responsiveFontSizes(mainTheme);
